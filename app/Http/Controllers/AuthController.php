4e23b7f4135c73bae79dde31e24de27766523c4f
Vinicius - Notebook <gvweb.solucoes@gmail.com>
2024-02-09 21:07:54 -0300

    View Lembrar Senha

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -25,7 +25,7 @@ class AuthController extends Controller
     }
 
     public function showResetRequestForm() {
-        return view('auth.lembrarSenha');
+        return view('auth.esqueciSenha');
     }
 
     public function showLoginForm()
