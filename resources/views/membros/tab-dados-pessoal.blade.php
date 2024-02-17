bb3e3939051e8ef5c4e8bc84015a2da42a61b264
Thiago Cardoso <thiagoc1415@gmail.com>
2024-02-17 10:09:17 -0300

    feat: adicionando e atualizando campos do formulário de membros

--- resources/views/membros/tab-dados-pessoal.blade.php
+++ resources/views/membros/tab-dados-pessoal.blade.php
@@ -3,10 +3,11 @@
     <div class="row mb-4">
       <!-- Coluna para foto do usuário -->
       <div class="col-xl-3 text-center">
-        <img src="https://via.placeholder.com/150" class="rounded-circle img-fluid mb-3" alt="Foto do usuário">
+        <img src="https://via.placeholder.com/150" id="user-picture" class="rounded-circle img-fluid mb-3" alt="Foto do usuário">
         <div>
-          <button class="btn btn-primary btn-sm">Trocar Foto</button>
-          <button class="btn btn-danger btn-sm">Apagar Foto</button>
+          <button class="btn btn-primary btn-sm" id="upload-picture">Trocar Foto</button>
+          <input type="file" class="d-none" id="upload-picture-input">
+          <button class="btn btn-danger btn-sm" id="delete-picture">Apagar Foto</button>
         </div>
       </div>
       <!-- Conteúdo do formulário -->
@@ -31,7 +32,7 @@
               
               <div class="col-xl-3">
                 <label for="nascimento">Dia de Nascimento</label>
-                <input type="date" class="form-control" id="nascimento" name="nascimento" value="{{ old('nascimento') }}">
+                <input type="date" class="form-control" id="nascimento" name="nascimento" value="{{ old('nascimento') }}" min="1900-01-01">
                 @error('nascimento')
                 <span class="help-block text-danger">{{ $message }}</span>
                 @enderror
@@ -74,7 +75,33 @@
               <div class="col-xl-3">
                 <label for="uf">UF</label>
                 <select class="form-control" id="uf" name="uf">
-                  <!-- Opções de UF -->
+                  <option value="AC">Acre</option>
+                  <option value="AL">Alagoas</option>
+                  <option value="AP">Amapá</option>
+                  <option value="AM">Amazonas</option>
+                  <option value="BA">Bahia</option>
+                  <option value="CE">Ceará</option>
+                  <option value="DF">Distrito Federal</option>
+                  <option value="ES">Espírito Santo</option>
+                  <option value="GO">Goiás</option>
+                  <option value="MA">Maranhão</option>
+                  <option value="MT">Mato Grosso</option>
+                  <option value="MS">Mato Grosso do Sul</option>
+                  <option value="MG">Minas Gerais</option>
+                  <option value="PA">Pará</option>
+                  <option value="PB">Paraíba</option>
+                  <option value="PR">Paraná</option>
+                  <option value="PE">Pernambuco</option>
+                  <option value="PI">Piauí</option>
+                  <option value="RJ">Rio de Janeiro</option>
+                  <option value="RN">Rio Grande do Norte</option>
+                  <option value="RS">Rio Grande do Sul</option>
+                  <option value="RO">Rondônia</option>
+                  <option value="RR">Roraima</option>
+                  <option value="SC">Santa Catarina</option>
+                  <option value="SP">São Paulo</option>
+                  <option value="SE">Sergipe</option>
+                  <option value="TO">Tocantins</option>
                 </select>
                 @error('uf')
                 <span class="help-block text-danger">{{ $message }}</span>
@@ -95,6 +122,15 @@
                 <label for="data-conversao">Data de Conversão</label>
                 <input type="date" class="form-control" id="data-conversao" name="data-conversao" value="{{ old('data-conversao') }}">
                 @error('data-conversao')
+                <span class="help-block text-danger">{{ $message }}</span>
+                @enderror
+              </div>
+            </div>
+            <div class="row mb-4">
+              <div class="col-xl-12">
+                <label for="history">Histórico</label>
+                <textarea class="form-control" aria-label="With textarea" id="history" name="history" value="{{ old('history') }}" style="resize: none; overflow: auto;"></textarea>
+                @error('history')
                 <span class="help-block text-danger">{{ $message }}</span>
                 @enderror
               </div>
