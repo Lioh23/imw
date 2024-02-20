48814951aaef9cd8c73cb39694df4245c10a41e9
Vinicius - Notebook <gvweb.solucoes@gmail.com>
2024-02-19 21:56:29 -0300

    View Formação JS

--- resources/views/congregados/tab-formacao.blade.php
+++ resources/views/congregados/tab-formacao.blade.php
@@ -34,20 +34,36 @@
                         <td>
                             <input type="text" class="form-control" name="curso-observacao[]"  value="{{ $formacaoEclesiastica->observacao }}">
                         </td>
-                        <td style="width: 100px;">
-                            <button type="button"
-                                class="btn btn-sm btn-secondary mr-2 btn-rounded apagar-linha-formacao">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                    stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2">
-                                    <polyline points="3 6 5 6 21 6"></polyline>
-                                    <path
-                                        d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2">
-                                    </path>
-                                    <line x1="10" y1="11" x2="10" y2="17"></line>
-                                    <line x1="14" y1="11" x2="14" y2="17"></line>
-                                </svg>
-                            </button>
+                        <td style="width: 200px;">
+                            <div class="centralizado">
+                                <!-- Botão Adicionar -->
+                                <button type="button" title="Adicionar Linha"
+                                    class="btn btn-sm btn-secondary mr-2 btn-rounded adicionar-formacaoEclesiastica">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                        stroke-linecap="round" stroke-linejoin="round"
+                                        class="feather feather-plus-circle">
+                                        <circle cx="12" cy="12" r="10"></circle>
+                                        <line x1="12" y1="8" x2="12" y2="16"></line>
+                                        <line x1="8" y1="12" x2="16" y2="12"></line>
+                                    </svg>
+                                </button>
+                                <!-- Botão Apagar -->
+                                <button type="button" title="Apagar Linha"
+                                    class="btn btn-sm btn-danger btn-rounded apagar-linha-formacaoEclesiastica">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                        stroke-linecap="round" stroke-linejoin="round"
+                                        class="feather feather-trash-2">
+                                        <polyline points="3 6 5 6 21 6"></polyline>
+                                        <path
+                                            d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2">
+                                        </path>
+                                        <line x1="10" y1="11" x2="10" y2="17"></line>
+                                        <line x1="14" y1="11" x2="14" y2="17"></line>
+                                    </svg>
+                                </button>
+                            </div>
                         </td>
                     </tr>
                     @endforeach
