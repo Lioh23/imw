2c3c18698dbf91f286e03abbb60aa68fe83fa488
Vinicius - Notebook <gvweb.solucoes@gmail.com>
2024-02-19 21:38:06 -0300

    Model

--- app/Models/MembresiaFormacaoEclesiastica.php
+++ app/Models/MembresiaFormacaoEclesiastica.php
@@ -12,7 +12,7 @@ class MembresiaFormacaoEclesiastica extends Model
 
     protected $table = 'membresia_formacoeseclesiasticas';
 
-    protected $fillable = ['inicio', 'observacao', 'termino', 'curso_id', 'membro_id'];
+    protected $fillable = ['inicio', 'termino', 'observacao', 'curso_id', 'membro_id'];
 
     public function membro()
     {
