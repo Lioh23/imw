077277caed42f9ad131e07ccdf4ec61349d74f5a
Vinicius - Notebook <gvweb.solucoes@gmail.com>
2024-02-19 21:11:36 -0300

    Co-authored-by: Aurélio Silva de Jesus <Lioh23@users.noreply.github.com>

---
+++ app/Models/MembresiaRolPermanente.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class MembresiaRolPermanente extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'membresia_rolpermanente';
+
+    protected $fillable = [
+        'status',
+        'numero_rol',
+        'codigo_host',
+        'dt_recepcao',
+        'dt_exclusao',
+        'clerigo_id',
+        'distrito_id',
+        'igreja_id',
+        'membro_id',
+        'modo_exclusao_id',
+        'modo_recepcao_id',
+        'regiao_id',
+        'congregacao_id'
+    ];
+}
