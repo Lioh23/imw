03826787aeca8ee40d8c477dba1c63cf8c9a53bc
Vinicius - Notebook <gvweb.solucoes@gmail.com>
2024-02-20 07:55:17 -0300

    Validaçcoes Congregado

--- app/Http/Requests/StoreCongregadoRequest.php
+++ app/Http/Requests/StoreCongregadoRequest.php
@@ -29,11 +29,19 @@ class StoreCongregadoRequest extends FormRequest
             'nome' => 'required',
             'sexo' => 'required',
             'data_nascimento' => ['required', new RangeDateRule],
+            'data_conversao' => [new RangeDateRule],
+            'data_batismo' => [new RangeDateRule],
+            'data_batismo_espirito' => [new RangeDateRule],
             'estado_civil' => 'required',
             'nacionalidade' => 'required',
             'naturalidade' => 'required',
             'uf' => 'required',
             'cpf' => ['required', new ValidaCPF],
+            'email_preferencial' => 'email|nullable',
+            'email_alternativo' => 'email|nullable',
+            'telefone_preferencial' => ['nullable', 'regex:/^(\d{10,11})$/'],
+            'telefone_alternativo' => ['nullable', 'regex:/^(\d{10,11})$/'],
+            'data_casamento' => [new RangeDateRule],
         ];
     }
 }
