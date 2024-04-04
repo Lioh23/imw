841b5b2f2a547ec6de3ee5fa14a62e78890ed33a
Vinicius - Notebook <gvweb.solucoes@gmail.com>
2024-04-04 00:34:22 -0300

    Danger

--- resources/views/financeiro/caixas/index.blade.php
+++ resources/views/financeiro/caixas/index.blade.php
@@ -46,8 +46,8 @@
                         <div class="col-12">
                             <a href="#" title="Inserir um novo registro"
                                 class="btn btn-primary right btn-rounded"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-square"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg> Novo </a>
-                            <table class="table table-striped" style="font-size: 90%; margin-top: 15px;">
-                                <thead class="thead-light">
+                                <table class="table mt-4">
+                                    <thead class="thead-light">
                                     <tr>
                                         <th>Descrição</th>
                                         <th>Tipo</th>
@@ -56,52 +56,19 @@
                                 </thead>
                                 <tbody>
                                     @foreach($caixas as $caixa)
-                                    <tr>
-                                        <td>{{$caixa->descricao}}</td>
-                                        <td>{{ match($caixa->tipo) { 'S' => 'Secundário', 'B' => 'Banco', 'P' => 'Principal', 'C' => 'Caixa', default => $caixa->tipo } }}</td>
+                                    <tr style="{{ $caixa->tipo == 'P' ? 'background-color: gray; color: white !important;' : '' }}">
+                                        <td style="{{ $caixa->tipo == 'P' ? 'color: white !important;' : '' }}">{{$caixa->descricao}}</td>
+                                        <td style="{{ $caixa->tipo == 'P' ? 'color: white !important;' : '' }}">{{ match($caixa->tipo) { 'S' => 'Secundário', 'B' => 'Banco', 'P' => 'Principal', 'C' => 'Caixa', default => $caixa->tipo } }}</td>
                                         <td class="table-action">
-                                            <a href="#" title="editar"
-                                                class="btn btn-rounded"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg></a>
-                                            <a href="#" title="apagar"
-                                                class="btn btn-rounded"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg></a>
-                                        </td>
+                                            @if($caixa->tipo != 'P')
+                                                <a href="#" title="editar"
+                                                    class="btn btn-sm btn-dark mr-2 btn-rounded mr-2 "><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg></a>
+                                                <a href="#" title="apagar"
+                                                    class="btn btn-sm btn-danger mr-2 btn-rounded"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg></a>
+                                            @endif
+                                          </td>
                                     </tr>
                                     @endforeach
-{{--                                     <tr>
-                                        <td>Caixa Comunicação</td>
-                                        <td>Secundário </td>
-                                        <td class="table-action">
-                                            <a href="#" title="editar"
-                                                class="btn btn-rounded"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg></a>
-                                            <a href="#" title="apagar"
-                                                class="btn btn-rounded"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg></a>
-                                        </td>
-                                    </tr>
-                                        <td>Caixa Missões (caixa bradesco)</td>
-                                        <td>Banco </td>
-                                        <td class="table-action">
-                                            <a href="#" title="editar"
-                                                class="btn btn-rounded"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg></a>
-                                            <a href="#" title="apagar"
-                                                class="btn btn-rounded"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg></a>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>Caixa Principal</td>
-                                        <td>Principal </td>
-                                        <td class="table-action">
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>Cozinha</td>
-                                        <td>Secundário </td>
-                                        <td class="table-action">
-                                            <a href="#" title="editar"
-                                                class="btn btn-rounded"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg></a>
-                                            <a href="#" title="apagar"
-                                                class="btn btn-rounded"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg></a>
-                                        </td>
-                                    </tr> --}}
                                 </tbody>
                             </table>
                             {{ $caixas->links('vendor.pagination.index') }}
