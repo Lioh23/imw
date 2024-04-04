8e298e6d317153c5fcfd2521922e841088a5da43
Aurélio Silva de Jesus <aurelin95@gmail.com>
2024-04-03 22:02:30 -0300

    fix: instituicoes

--- app/Http/Controllers/InstituicaoController.php
+++ app/Http/Controllers/InstituicaoController.php
@@ -9,14 +9,9 @@ class InstituicaoController extends Controller
 {
     public function index(Request $request)
     {
-        $query = InstituicoesInstituicao::where(function ($query) {
-            $query->where('id', session()->get('session_perfil')->instituicao_id)
-                  ->orWhere('instituicao_pai_id', session()->get('session_perfil')->instituicao_id);
-        });
-
-        if ($request->has('search') && !empty($request->search)) {
+        $query = InstituicoesInstituicao::when($request->has('search') && !empty($request->search), function($query) use ($request) {
             $query->where('nome', 'like', '%' . $request->search . '%');
-        }
+        });
     
         $instituicoes = $query->paginate(10);
         return response()->json($instituicoes);
