b6bcadc912f70a428850687f40fdb8997f30962c
Vinicius - Notebook <gvweb.solucoes@gmail.com>
2024-04-13 01:54:16 -0300

    Financeiro Movimento Caixa

--- app/Services/ServiceFinanceiro/StoreLancamentoEntradaService.php
+++ app/Services/ServiceFinanceiro/StoreLancamentoEntradaService.php
@@ -2,7 +2,10 @@
 
 namespace App\Services\ServiceFinanceiro;
 
+use App\Models\FinanceiroFornecedores;
 use App\Models\FinanceiroLancamento;
+use App\Models\MembresiaMembro;
+use App\Models\PessoasPessoa;
 use Carbon\Carbon;
 
 class StoreLancamentoEntradaService
@@ -18,7 +21,7 @@ class StoreLancamentoEntradaService
        
         $tipoPaganteFavorecidoId = $data['tipo_pagante_favorecido_id'];
         $paganteFavorecido = $data['pagante_favorecido'];
-        
+
         $lancamentos = [
             'data_lancamento' => Carbon::now()->format('Y-m-d'),
             'valor' => str_replace(',', '.', $data['valor']),
@@ -30,17 +33,33 @@ class StoreLancamentoEntradaService
             'caixa_id' => $data['caixa_id'],
             'instituicao_id' => session()->get('session_perfil')->instituicao_id
         ];
-        
-        if ($tipoPaganteFavorecidoId == 1) {
-            $lancamentos['membro_id'] = $paganteFavorecido;
-        } elseif ($tipoPaganteFavorecidoId == 2) {
-            $lancamentos['fornecedores_id'] = $paganteFavorecido;
-        } elseif ($tipoPaganteFavorecidoId == 3) {
-            $lancamentos['clerigo_id'] = $paganteFavorecido;
-        } else {
-            $lancamentos['pagante_favorecido'] = $paganteFavorecido;
+
+        $paganteFavorecidoModel = null;
+        $campoId = null;
+
+        switch ($tipoPaganteFavorecidoId) {
+            case 1:
+                $paganteFavorecidoModel = MembresiaMembro::find($paganteFavorecido);
+                $campoId = 'membro_id';
+                break;
+            case 2:
+                $paganteFavorecidoModel = FinanceiroFornecedores::find($paganteFavorecido);
+                $campoId = 'fornecedores_id';
+                break;
+            case 3:
+                $paganteFavorecidoModel = PessoasPessoa::find($paganteFavorecido);
+                $campoId = 'clerigo_id';
+                break;
+            default:
+                $lancamentos['pagante_favorecido'] = $paganteFavorecido;
+                break;
+        }
+
+        if ($paganteFavorecidoModel) {
+            $lancamentos['pagante_favorecido'] = $paganteFavorecidoModel->nome;
+            $lancamentos[$campoId] = $paganteFavorecido;
         }
 
         FinanceiroLancamento::create($lancamentos);
     }
-}
+} 
