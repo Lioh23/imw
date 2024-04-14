36656a1335111815565ab91229b6f50dabd29e3f
Vinicius - Notebook <gvweb.solucoes@gmail.com>
2024-04-13 23:46:13 -0300

    Layout anexos Request

--- app/Http/Requests/FinanceiroStoreSaidaRequest.php
+++ app/Http/Requests/FinanceiroStoreSaidaRequest.php
@@ -23,13 +23,19 @@ class FinanceiroStoreSaidaRequest extends FormRequest
      */
     public function rules()
     {
-         return [
+        return [
             'plano_conta_id' => 'required',
             'caixa_id'       => 'required',
             'valor'          => 'required',
             'data_movimento' => 'required',
             'descricao'      => 'required',
-            'tipo_pagante_favorecido_id' => 'required'
+            'tipo_pagante_favorecido_id' => 'required',
+            'anexo1' => 'nullable|file|max:2048|mimes:pdf,doc,docx',
+            'descricao_anexo1' => 'nullable|string|max:255',
+            'anexo2' => 'nullable|file|max:2048|mimes:pdf,doc,docx',
+            'descricao_anexo2' => 'nullable|string|max:255',
+            'anexo3' => 'nullable|file|max:2048|mimes:pdf,doc,docx',
+            'descricao_anexo3' => 'nullable|string|max:255',
         ];
     }
 }
