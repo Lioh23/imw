aa31f74ac3546ecea3f310b3878a0a9b1ba518dd
Vinicius - Notebook <gvweb.solucoes@gmail.com>
2024-04-29 05:24:08 -0300

    Livro Caixa

--- resources/views/financeiro/relatorios/livrocaixa.blade.php
+++ resources/views/financeiro/relatorios/livrocaixa.blade.php
@@ -70,142 +70,144 @@
         </div>
     </div>
 
-    <div class="col-lg-12 col-12 layout-spacing">
-        <div class="statbox widget box box-shadow">
-            <div class="widget-content widget-content-area">
-                <!-- Conteúdo -->
-                <div class="card mb-3">
-                    <div class="card-body">
-                        <div class="row">
+    @if(request()->input('dt'))
+        <div class="col-lg-12 col-12 layout-spacing">
+            <div class="statbox widget box box-shadow">
+                <div class="widget-content widget-content-area">
+                    <!-- Conteúdo -->
+                    <div class="card mb-3">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-12">
+                                    <h5 class="mt-3">Discriminação de saldos por caixa</h5>
+                                    <table class="table table-striped" style="font-size: 90%; margin-top: 15px;">
+                                        <thead class="thead-dark">
+                                            <tr>
+                                                <th>CAIXA</th>
+                                                <th width="300" style="text-align: right">SALDO ANTERIOR CONSOLIDADO</th>
+                                                <th width="120" style="text-align: right">TOTAIS DE ENTRADAS</th>
+                                                <th width="120" style="text-align: right">TOTAIS DE SAÍDAS</th>
+                                                <th width="120" style="text-align: right">TRANSF. ENTRADAS</th>
+                                                <th width="120" style="text-align: right">TRANSF. SAÍDAS</th>
+                                                <th width="120" style="text-align: right">SALDO ATUAL</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @php
+                                                $totalSaldoFinal = 0;
+                                                $totalEntradas = 0;
+                                                $totalSaidas = 0;
+                                                $totalTransferenciasEntrada = 0;
+                                                $totalTransferenciasSaida = 0;
+                                                $totalSaldoAtual = 0;
+                                            @endphp
+
+                                            @foreach ($caixas as $caixa)
+                                                <tr>
+                                                    <td style="text-align: left">{{ $caixa->caixa }}</td>
+                                                    <td style="text-align: right">
+                                                        {{ 'R$ ' . number_format($caixa->saldo_final, 2, ',', '.') }}</td>
+                                                    <td style="text-align: right">
+                                                        {{ 'R$ ' . number_format($caixa->total_entradas, 2, ',', '.') }}</td>
+                                                    <td style="text-align: right">
+                                                        {{ 'R$ ' . ($caixa->total_saidas > 0 ? '-' : '') . number_format(abs($caixa->total_saidas), 2, ',', '.') }}
+                                                    </td>
+                                                    <td style="text-align: right">
+                                                        {{ 'R$ ' . number_format($caixa->total_transferencias_entrada, 2, ',', '.') }}
+                                                    </td>
+                                                    <td style="text-align: right">
+                                                        {{ 'R$ ' . ($caixa->total_transferencias_saida > 0 ? '-' : '') . number_format(abs($caixa->total_transferencias_saida), 2, ',', '.') }}
+                                                    </td>
+                                                    <td style="text-align: right">
+                                                        {{ 'R$ ' . number_format($caixa->saldo_atual, 2, ',', '.') }}</td>
+                                                </tr>
+                                                @php
+                                                    $totalSaldoFinal += $caixa->saldo_final;
+                                                    $totalEntradas += $caixa->total_entradas;
+                                                    $totalSaidas += $caixa->total_saidas;
+                                                    $totalTransferenciasEntrada += $caixa->total_transferencias_entrada;
+                                                    $totalTransferenciasSaida += $caixa->total_transferencias_saida;
+                                                    $totalSaldoAtual += $caixa->saldo_atual;
+                                                @endphp
+                                            @endforeach
+
+                                            {{-- Total de cada caixa --}}
+                                            <tr>
+                                                <td style="text-align: left"><strong>Total dos Caixas</strong></td>
+                                                <td style="text-align: right">
+                                                    <strong>{{ 'R$ ' . number_format($totalSaldoFinal, 2, ',', '.') }}</strong>
+                                                </td>
+                                                <td style="text-align: right">
+                                                    <strong>{{ 'R$ ' . number_format($totalEntradas, 2, ',', '.') }}</strong>
+                                                </td>
+                                                <td style="text-align: right">
+                                                    <strong>{{ 'R$ ' . ($totalSaidas > 0 ? '-' : '') . number_format(abs($totalSaidas), 2, ',', '.') }}</strong>
+                                                </td>
+                                                <td style="text-align: right">
+                                                    <strong>{{ 'R$ ' . number_format($totalTransferenciasEntrada, 2, ',', '.') }}</strong>
+                                                </td>
+                                                <td style="text-align: right">
+                                                    <strong>{{ 'R$ ' . ($totalTransferenciasSaida > 0 ? '-' : '') . number_format(abs($totalTransferenciasSaida), 2, ',', '.') }}</strong>
+                                                </td>
+                                                <td style="text-align: right">
+                                                    <strong>{{ 'R$ ' . number_format($totalSaldoAtual, 2, ',', '.') }}</strong>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                            {{-- <div class="row">
+                                <div class="col-12 text-center">
+                                    <button class="btn btn-success btn-rounded" onclick="exportReportToExcel();"><i
+                                            class="fa fa-file-excel" aria-hidden="true"></i> Exportar</button>
+                                </div>
+                            </div> --}}
+
+                            <div class="col-12 mt-3">
+                                <h5>Discriminação dos Lançamentos por Conta</h5>
+                            </div>
                             <div class="col-12">
-                                <h5 class="mt-3">Discriminação de saldos por caixa</h5>
-                                <table class="table table-striped" style="font-size: 90%; margin-top: 15px;">
+                                <table class="table" style="font-size: 90%; margin-top: 15px;">
                                     <thead class="thead-dark">
                                         <tr>
+                                            <th>CONTA</th>
                                             <th>CAIXA</th>
-                                            <th width="300" style="text-align: right">SALDO ANTERIOR CONSOLIDADO</th>
-                                            <th width="120" style="text-align: right">TOTAIS DE ENTRADAS</th>
-                                            <th width="120" style="text-align: right">TOTAIS DE SAÍDAS</th>
-                                            <th width="120" style="text-align: right">TRANSF. ENTRADAS</th>
-                                            <th width="120" style="text-align: right">TRANSF. SAÍDAS</th>
-                                            <th width="120" style="text-align: right">SALDO ATUAL</th>
+                                            <th width="100">TOTAL</th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        @php
-                                            $totalSaldoFinal = 0;
-                                            $totalEntradas = 0;
-                                            $totalSaidas = 0;
-                                            $totalTransferenciasEntrada = 0;
-                                            $totalTransferenciasSaida = 0;
-                                            $totalSaldoAtual = 0;
-                                        @endphp
-
-                                        @foreach ($caixas as $caixa)
-                                            <tr>
-                                                <td style="text-align: left">{{ $caixa->caixa }}</td>
-                                                <td style="text-align: right">
-                                                    {{ 'R$ ' . number_format($caixa->saldo_final, 2, ',', '.') }}</td>
-                                                <td style="text-align: right">
-                                                    {{ 'R$ ' . number_format($caixa->total_entradas, 2, ',', '.') }}</td>
-                                                <td style="text-align: right">
-                                                    {{ 'R$ ' . ($caixa->total_saidas > 0 ? '-' : '') . number_format(abs($caixa->total_saidas), 2, ',', '.') }}
-                                                </td>
-                                                <td style="text-align: right">
-                                                    {{ 'R$ ' . number_format($caixa->total_transferencias_entrada, 2, ',', '.') }}
-                                                </td>
-                                                <td style="text-align: right">
-                                                    {{ 'R$ ' . ($caixa->total_transferencias_saida > 0 ? '-' : '') . number_format(abs($caixa->total_transferencias_saida), 2, ',', '.') }}
-                                                </td>
-                                                <td style="text-align: right">
-                                                    {{ 'R$ ' . number_format($caixa->saldo_atual, 2, ',', '.') }}</td>
-                                            </tr>
-                                            @php
-                                                $totalSaldoFinal += $caixa->saldo_final;
-                                                $totalEntradas += $caixa->total_entradas;
-                                                $totalSaidas += $caixa->total_saidas;
-                                                $totalTransferenciasEntrada += $caixa->total_transferencias_entrada;
-                                                $totalTransferenciasSaida += $caixa->total_transferencias_saida;
-                                                $totalSaldoAtual += $caixa->saldo_atual;
-                                            @endphp
+                                        @php $numerosJaExibidos = []; @endphp
+                                        @foreach ($lancamentos as $index => $lancamento)
+                                            @if (!in_array($lancamento->numeracao, $numerosJaExibidos))
+                                                <tr>
+                                                    <td rowspan="{{ count(array_filter($lancamentos, function($item) use ($lancamento) {
+                                                        return $item->numeracao === $lancamento->numeracao;
+                                                    })) }}" style="font-weight: bold">
+                                                        {{ $lancamento->numeracao }} - {{ $lancamento->nome }}
+                                                    </td>
+                                                    <td>{{ $lancamento->caixa }}</td>
+                                                    <td>R$ {{ number_format($lancamento->total, 2, ',', '.') }}</td>
+                                                </tr>
+                                                @php $numerosJaExibidos[] = $lancamento->numeracao; @endphp
+                                            @else
+                                                <tr>
+                                                    <td>{{ $lancamento->caixa }}</td>
+                                                    <td>R$ {{ number_format($lancamento->total, 2, ',', '.') }}</td>
+                                                </tr>
+                                            @endif
                                         @endforeach
-
-                                        {{-- Total de cada caixa --}}
-                                        <tr>
-                                            <td style="text-align: left"><strong>Total dos Caixas</strong></td>
-                                            <td style="text-align: right">
-                                                <strong>{{ 'R$ ' . number_format($totalSaldoFinal, 2, ',', '.') }}</strong>
-                                            </td>
-                                            <td style="text-align: right">
-                                                <strong>{{ 'R$ ' . number_format($totalEntradas, 2, ',', '.') }}</strong>
-                                            </td>
-                                            <td style="text-align: right">
-                                                <strong>{{ 'R$ ' . ($totalSaidas > 0 ? '-' : '') . number_format(abs($totalSaidas), 2, ',', '.') }}</strong>
-                                            </td>
-                                            <td style="text-align: right">
-                                                <strong>{{ 'R$ ' . number_format($totalTransferenciasEntrada, 2, ',', '.') }}</strong>
-                                            </td>
-                                            <td style="text-align: right">
-                                                <strong>{{ 'R$ ' . ($totalTransferenciasSaida > 0 ? '-' : '') . number_format(abs($totalTransferenciasSaida), 2, ',', '.') }}</strong>
-                                            </td>
-                                            <td style="text-align: right">
-                                                <strong>{{ 'R$ ' . number_format($totalSaldoAtual, 2, ',', '.') }}</strong>
-                                            </td>
-                                        </tr>
                                     </tbody>
                                 </table>
+                                
                             </div>
-                        </div>
-                        {{-- <div class="row">
-                            <div class="col-12 text-center">
-                                <button class="btn btn-success btn-rounded" onclick="exportReportToExcel();"><i
-                                        class="fa fa-file-excel" aria-hidden="true"></i> Exportar</button>
-                            </div>
-                        </div> --}}
-
-                        <div class="col-12 mt-3">
-                            <h5>Discriminação dos Lançamentos por Conta</h5>
-                        </div>
-                        <div class="col-12">
-                            <table class="table" style="font-size: 90%; margin-top: 15px;">
-                                <thead class="thead-dark">
-                                    <tr>
-                                        <th>CONTA</th>
-                                        <th>CAIXA</th>
-                                        <th width="100">TOTAL</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @php $numerosJaExibidos = []; @endphp
-                                    @foreach ($lancamentos as $index => $lancamento)
-                                        @if (!in_array($lancamento->numeracao, $numerosJaExibidos))
-                                            <tr>
-                                                <td rowspan="{{ count(array_filter($lancamentos, function($item) use ($lancamento) {
-                                                    return $item->numeracao === $lancamento->numeracao;
-                                                })) }}" style="font-weight: bold">
-                                                    {{ $lancamento->numeracao }} - {{ $lancamento->nome }}
-                                                </td>
-                                                <td>{{ $lancamento->caixa }}</td>
-                                                <td>R$ {{ number_format($lancamento->total, 2, ',', '.') }}</td>
-                                            </tr>
-                                            @php $numerosJaExibidos[] = $lancamento->numeracao; @endphp
-                                        @else
-                                            <tr>
-                                                <td>{{ $lancamento->caixa }}</td>
-                                                <td>R$ {{ number_format($lancamento->total, 2, ',', '.') }}</td>
-                                            </tr>
-                                        @endif
-                                    @endforeach
-                                </tbody>
-                            </table>
-                            
                         </div>
                     </div>
+                    <!-- Fim do Conteúdo -->
                 </div>
-                <!-- Fim do Conteúdo -->
             </div>
         </div>
-    </div>
+    @endif
 
 @section('extras-scripts')
     <script>
