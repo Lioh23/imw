96d092f2c967db0b2d127d80362870414ac2010a
Vinicius de Oliveira Almeida <valmeida@idea-planejamento.com>
2024-05-08 06:13:17 -0300

    Admin

--- resources/views/usuarios/index.blade.php
+++ resources/views/usuarios/index.blade.php
@@ -107,14 +107,14 @@
                                         </div>
                                     </td>
             <td class="text-center">
-                <a href="{{ route('usuarios.editar', $usuario->id) }}" title="Editar"
+                {{-- <a href="{{ route('usuarios.editar', $usuario->id) }}" title="Editar"
                     class="btn btn-sm btn-dark mr-2 btn-rounded">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                         stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                         class="feather feather-edit-2">
                         <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path>
                     </svg>
-                </a>
+                </a> --}}
                {{--  <form action="{{ route('usuarios.deletar', $usuario->id) }}" method="POST" style="display: inline-block;"
                     id="form_delete_usuario_{{ $index }}">
                     @csrf
