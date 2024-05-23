24937e0a1a0e0d51ffcb00724a3ff10e4838fe7e
Vinicius de Oliveira Almeida <valmeida@idea-planejamento.com>
2024-05-22 22:49:35 -0300

    Admin deletar

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -122,7 +122,7 @@
                         <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path>
                     </svg>
                 </a>
-                <form action="{{ route('usuarios.deletar', $usuario->id) }}" method="POST" style="display: inline-block;"
+                <form action="{{ route('admin.deletar', $usuario->id) }}" method="POST" style="display: inline-block;"
                     id="form_delete_usuario_{{ $index }}">
                     @csrf
                     @method('DELETE')
