c34d391501f44de5261b3ea002a1697ff98fba95
Prof Vinicius <gvweb.solucoes@gmail.com>
2024-05-31 01:06:21 -0300

    Cards Membros

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,9 +5,11 @@ namespace App\Http\Controllers;
 use App\Models\MembresiaMembro;
 use App\Models\PerfilUser;
 use App\Services\ServicePerfil\IdentificaPerfilService;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -15,23 +17,64 @@ class HomeController extends Controller
     {
         $igrejaId = session()->get('session_perfil')->instituicao_id;
         $statusAtivo = 'A';
-    
-        $counts = MembresiaMembro::selectRaw("
-                SUM(CASE WHEN vinculo = 'M' THEN 1 ELSE 0 END) as activeMembrosCount,
-                SUM(CASE WHEN vinculo = 'C' THEN 1 ELSE 0 END) as activeCongregadosCount,
-                SUM(CASE WHEN vinculo = 'V' THEN 1 ELSE 0 END) as activeVisitantesCount
-            ")
+        $statusInativo = 'I';
+
+        $activeMembrosCount = MembresiaMembro::where('status', $statusAtivo)
+            ->where('vinculo', 'M')
+            ->where('igreja_id', $igrejaId)
+            ->count();
+
+        $activeCongregadosCount = MembresiaMembro::where('status', $statusAtivo)
+            ->where('vinculo', 'C')
+            ->where('igreja_id', $igrejaId)
+            ->count();
+
+        $activeVisitantesCount = MembresiaMembro::where('status', $statusAtivo)
+            ->where('vinculo', 'V')
+            ->where('igreja_id', $igrejaId)
+            ->count();
+
+        $totalAtivos = MembresiaMembro::where('status', $statusAtivo)
+            ->where('vinculo', 'M')
+            ->where('igreja_id', $igrejaId)
+            ->count();
+
+        $totalInativos = MembresiaMembro::where('status', $statusInativo)
+            ->where('vinculo', 'M')
+            ->where('igreja_id', $igrejaId)
+            ->count();
+
+        $visitantesPorMes = MembresiaMembro::select(
+                DB::raw('MONTH(created_at) as month'),
+                DB::raw('COUNT(*) as count')
+            )
+            ->where('vinculo', 'V')
             ->where('status', $statusAtivo)
             ->where('igreja_id', $igrejaId)
-            ->first();
+            ->whereYear('created_at', Carbon::now()->year)
+            ->groupBy(DB::raw('MONTH(created_at)'))
+            ->orderBy(DB::raw('MONTH(created_at)'))
+            ->get()
+            ->pluck('count', 'month')
+            ->toArray();
     
+     
+        $visitantesPorMesCompleto = array_fill(1, 12, 0);
+        foreach ($visitantesPorMes as $month => $count) {
+            $visitantesPorMesCompleto[$month] = $count;
+        }
+
         return view('dashboard', [
-            'activeMembrosCount' => $counts->activeMembrosCount,
-            'activeCongregadosCount' => $counts->activeCongregadosCount,
-            'activeVisitantesCount' => $counts->activeVisitantesCount,
+            'activeMembrosCount' => $activeMembrosCount,
+            'activeCongregadosCount' => $activeCongregadosCount,
+            'activeVisitantesCount' => $activeVisitantesCount,
+            'totalAtivos' => $totalAtivos,
+            'totalInativos' => $totalInativos,
+            'visitantesPorMes' => $visitantesPorMesCompleto,
         ]);
     }
-    
+
+
     public function selecionarPerfil()
     {
         // Obter o ID do usuário autenticado
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,12 @@
 @extends('template.layout')
+@section('extras-css')
+<style>
+     .chart-container {
+            position: relative;
+            height: 400px; /* Ajuste a altura conforme necessário */
+        }
+</style>
+@endsection
 
 @section('content')
 <div class="container-fluid h-100">
@@ -33,83 +41,45 @@
 
     <!-- Gráficos -->
     <div class="row flex-fill mt-4">
-        <!-- Gráfico de Contribuições Mensais -->
-        <div class="col-md-6">
-            <div class="card h-100">
-                <div class="card-body">
-                    <h6 class="card-title">Gráfico de Contribuições Mensais</h6>
-                    <canvas id="contribuicoesChart"></canvas>
-                </div>
-            </div>
-        </div>
-
-        <!-- Gráfico de Membros Ativos vs Inativos -->
-        <div class="col-md-6">
-            <div class="card h-100">
-                <div class="card-body">
-                    <h6 class="card-title">Gráfico de Membros Ativos vs Inativos</h6>
+         <!-- Gráfico de Membros Ativos vs Inativos -->
+    <div class="col-md-6">
+        <div class="card h-100">
+            <div class="card-body">
+                <h6 class="card-title">Gráfico de Membros Ativos vs Inativos</h6>
+                <div class="chart-container">
                     <canvas id="membrosChart"></canvas>
                 </div>
             </div>
         </div>
     </div>
 
-    <div class="row flex-fill mt-4">
-        <!-- Gráfico de Eventos por Mês -->
-        <div class="col-md-6">
-            <div class="card h-100">
-                <div class="card-body">
-                    <h6 class="card-title">Gráfico de Eventos por Mês</h6>
-                    <canvas id="eventosChart"></canvas>
-                </div>
-            </div>
-        </div>
-
-        <!-- Gráfico de Doações por Categoria -->
-        <div class="col-md-6">
-            <div class="card h-100">
-                <div class="card-body">
-                    <h6 class="card-title">Gráfico de Doações por Categoria</h6>
-                    <canvas id="doacoesChart"></canvas>
+    <!-- Gráfico de Visitantes por Mês -->
+    <div class="col-md-6">
+        <div class="card h-100">
+            <div class="card-body">
+                <h6 class="card-title">Gráfico de Visitantes por Mês</h6>
+                <div class="chart-container">
+                    <canvas id="visitantesChart"></canvas>
                 </div>
             </div>
         </div>
     </div>
+    </div>
+
+  
 </div>
 
 <!-- Incluir scripts para os gráficos (por exemplo, Chart.js) -->
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <script>
-    // Dados dos gráficos
-    var ctx1 = document.getElementById('contribuicoesChart').getContext('2d');
-    var contribuicoesChart = new Chart(ctx1, {
-        type: 'line',
-        data: {
-            labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun'],
-            datasets: [{
-                label: 'Contribuições',
-                data: [2000, 2200, 2100, 2300, 2400, 2500],
-                backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                borderColor: 'rgba(75, 192, 192, 1)',
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-        }
-    });
 
     var ctx2 = document.getElementById('membrosChart').getContext('2d');
     var membrosChart = new Chart(ctx2, {
         type: 'pie',
         data: {
-            labels: ['Ativos', 'Inativos'],
+            labels: ['Inativos', 'Ativos'],
             datasets: [{
-                data: [150, 50],
+                data: [{{ $totalInativos }}, {{ $totalAtivos }}],
                 backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)'],
                 borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)'],
                 borderWidth: 1
@@ -117,40 +87,40 @@
         }
     });
 
-    var ctx3 = document.getElementById('eventosChart').getContext('2d');
-    var eventosChart = new Chart(ctx3, {
-        type: 'bar',
-        data: {
-            labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun'],
-            datasets: [{
-                label: 'Eventos',
-                data: [2, 3, 4, 3, 5, 4],
-                backgroundColor: 'rgba(255, 206, 86, 0.2)',
-                borderColor: 'rgba(255, 206, 86, 1)',
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
+    var ctx3 = document.getElementById('visitantesChart').getContext('2d');
+        var visitantesChart = new Chart(ctx3, {
+            type: 'bar',
+            data: {
+                labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+                datasets: [{
+                    label: 'Visitantes',
+                    data: [
+                        {{ $visitantesPorMes[1] }},
+                        {{ $visitantesPorMes[2] }},
+                        {{ $visitantesPorMes[3] }},
+                        {{ $visitantesPorMes[4] }},
+                        {{ $visitantesPorMes[5] }},
+                        {{ $visitantesPorMes[6] }},
+                        {{ $visitantesPorMes[7] }},
+                        {{ $visitantesPorMes[8] }},
+                        {{ $visitantesPorMes[9] }},
+                        {{ $visitantesPorMes[10] }},
+                        {{ $visitantesPorMes[11] }},
+                        {{ $visitantesPorMes[12] }}
+                    ],
+                    backgroundColor: 'rgba(255, 206, 86, 0.2)',
+                    borderColor: 'rgba(255, 206, 86, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
                 }
             }
-        }
-    });
+        });
 
-    var ctx4 = document.getElementById('doacoesChart').getContext('2d');
-    var doacoesChart = new Chart(ctx4, {
-        type: 'doughnut',
-        data: {
-            labels: ['Dízimos', 'Ofertas', 'Missões'],
-            datasets: [{
-                data: [5000, 3000, 2000],
-                backgroundColor: ['rgba(75, 192, 192, 0.2)', 'rgba(255, 99, 132, 0.2)', 'rgba(255, 206, 86, 0.2)'],
-                borderColor: ['rgba(75, 192, 192, 1)', 'rgba(255, 99, 132, 1)', 'rgba(255, 206, 86, 1)'],
-                borderWidth: 1
-            }]
-        }
-    });
 </script>
 @endsection
