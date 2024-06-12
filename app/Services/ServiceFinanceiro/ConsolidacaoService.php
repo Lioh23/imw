a95cef41f331a9f80847ec4fd08e583b63b5a29e
Prof Vinicius <gvweb.solucoes@gmail.com>
2024-06-12 01:37:47 -0300

    Consolidacao

--- app/Services/ServiceFinanceiro/ConsolidacaoService.php
+++ app/Services/ServiceFinanceiro/ConsolidacaoService.php
@@ -5,8 +5,10 @@ namespace App\Services\ServiceFinanceiro;
 use App\Models\FinanceiroFornecedores;
 use App\Models\FinanceiroTipoPaganteFavorecido;
 use App\Models\MembresiaMembro;
+use App\Services\ServiceFinanceiroRelatorios\LivroCaixaService;
 use App\Traits\FinanceiroUtils;
 use App\Traits\Identifiable;
+use Carbon\Carbon;
 
 class ConsolidacaoService
 {
@@ -15,10 +17,16 @@ class ConsolidacaoService
 
     public function execute()
     {
-        return [
-            'caixas' => FinanceiroUtils::caixas(),
-            'ultimoCaixa' => FinanceiroUtils::ultimoCaixaConciliado(),
-            'lancamentosPorConta' => FinanceiroUtils::lancamentosPorContas()
-        ];
+        $oldCaixa = FinanceiroUtils::ultimoCaixaConciliado();
+        $formattedDate = $oldCaixa instanceof Carbon ? $oldCaixa->format('m/Y') : null;
+
+        // Capture o array retornado por LivroCaixaService
+        $data = app(LivroCaixaService::class)->execute($formattedDate, 'all');
+        
+        // Adicione o novo item ao array
+        $data['ultimoCaixa'] = $oldCaixa;
+
+        // Retorne o array modificado
+        return $data;
     }
 }
