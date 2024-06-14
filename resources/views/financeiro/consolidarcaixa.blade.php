a5a866167735c96080cddf8558bf95974516150b
Prof Vinicius <gvweb.solucoes@gmail.com>
2024-06-14 00:08:57 -0300

    Financeiro

--- resources/views/financeiro/consolidarcaixa.blade.php
+++ resources/views/financeiro/consolidarcaixa.blade.php
@@ -15,25 +15,17 @@
         color: white !important;
     }
 
-    .badge {
-        padding: 10px;
-        border-radius: 5px;
-        font-weight: bold;
+    .modal-body ul {
+        padding-left: 20px;
     }
 
-    .danger {
-        background-color: red;
-        color: white;
+    .modal-body ul li {
+        word-wrap: break-word;
+        white-space: normal;
     }
 
-    .gray {
-        background-color: gray;
-        color: white;
-    }
-
-    .green {
-        background-color: green;
-        color: white;
+    .badge-danger {
+        font-weight: normal;
     }
 </style>
 @endsection
@@ -45,6 +37,28 @@
 
 @section('content')
 @include('extras.alerts')
+<!-- Modal -->
+<div class="modal fade" id="alertModal" tabindex="-1" role="dialog" aria-labelledby="alertModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document"> <!-- Use modal-xl para um modal ainda maior -->
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="alertModalLabel">Atenção!</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body" style="max-height: 400px; overflow-y: auto;">
+                <ul id="alertMessages">
+                    <!-- Alert messages will be displayed here -->
+                </ul>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+            </div>
+        </div>
+    </div>
+</div>
+
 <div class="container-fluid">
 
 </div>
@@ -85,7 +99,7 @@
                                         <td style="text-align: right">
                                             {{ 'R$ ' . number_format($caixa->saldo_final, 2, ',', '.') }}
                                         </td>
-                                        <td style="text-align: right">
+                                        <td class="total-entradas" style="text-align: right">
                                             {{ 'R$ ' . number_format($caixa->total_entradas, 2, ',', '.') }}
                                         </td>
                                         <td style="text-align: right">
@@ -181,8 +195,7 @@
                             @csrf
                             <input type="hidden" name="ano" value="{{ \Carbon\Carbon::parse($ultimoCaixa)->addMonth()->year }}" />
                             <input type="hidden" name="mes" value="{{ \Carbon\Carbon::parse($ultimoCaixa)->addMonth()->month }}" />
-                            <button data-form-id="form_consolidacao_automatica" class="btn btn-success p-2 btn-rounded btn-confirm" style="text-transform: uppercase;" type="submit">CONSOLIDAR
-                                {{ \Carbon\Carbon::parse($ultimoCaixa)->addMonth()->isoFormat('MMMM [de] YYYY') }}</button>
+                            <button data-form-id="form_consolidacao_automatica" class="btn btn-success p-2 btn-rounded btn-confirm" style="text-transform: uppercase;" type="button" disabled>CONSOLIDAR {{ \Carbon\Carbon::parse($ultimoCaixa)->addMonth()->isoFormat('MMMM [de] YYYY') }}</button>
                         </form>
 
                     </div>
@@ -194,20 +207,90 @@
     </div>
 </div>
 <script>
-    $(document).ready(function() {
-        $('.saldo-atual').each(function() {
-            var saldoText = $(this).text().replace('R$ ', '').replace('.', '').replace(',', '.');
-            var saldo = parseFloat(saldoText);
+   $(document).ready(function() {
+    // Adiciona badges conforme o saldo atual
+    $('.saldo-atual').each(function() {
+        var saldoText = $(this).text().replace('R$ ', '').replace('.', '').replace(',', '.');
+        var saldo = parseFloat(saldoText);
 
-            if (saldo < 0) {
-                $(this).html('<span class="badge badge-danger">' + $(this).html() + '</span>');
-            } else if (saldo === 0) {
-                $(this).html('<span class="badge badge-secondary">' + $(this).html() + '</span>');
-            } else {
-                $(this).html('<span class="badge badge-success">' + $(this).html() + '</span>');
+        if (saldo < 0) {
+            $(this).html('<span class="badge badge-danger">' + $(this).html() + '</span>');
+        } else if (saldo === 0) {
+            $(this).html('<span class="badge badge-secondary">' + $(this).html() + '</span>');
+        } else {
+            $(this).html('<span class="badge badge-success">' + $(this).html() + '</span>');
+        }
+    });
+
+    // Verificações das regras
+    let shouldSubmit = true;
+    let entradasNegativasOuZero = [];
+    let saldosNegativos = [];
+
+    // Verifica totais de entradas
+    $('.total-entradas').each(function() {
+        var entradaText = $(this).text().replace('R$ ', '').replace('.', '').replace(',', '.');
+        var entrada = parseFloat(entradaText);
+        var caixa = $(this).closest('tr').find('td:first').text();
+
+        if (entrada <= 0) {
+            entradasNegativasOuZero.push(caixa);
+            shouldSubmit = false;
+        }
+    });
+
+    // Verifica saldo atual
+    $('.saldo-atual').each(function() {
+        var saldoText = $(this).text().replace('R$ ', '').replace('.', '').replace(',', '.');
+        var saldo = parseFloat(saldoText);
+        var caixa = $(this).closest('tr').find('td:first').text();
+
+        if (saldo < 0) {
+            saldosNegativos.push(caixa);
+            shouldSubmit = false;
+        }
+    });
+
+    if (!shouldSubmit) {
+        let alertList = '';
+
+        if (entradasNegativasOuZero.length > 0) {
+            alertList += `<li><h3 class="badge badge-default">OS SEGUINTES CAIXAS NÃO PODEM TER ENTRADAS NEGATIVAS OU ZERO:</h3><ul>${entradasNegativasOuZero.map(caixa => `<li>${caixa}</li>`).join('')}</ul></li>`;
+        }
+
+        if (saldosNegativos.length > 0) {
+            alertList += `<li><h3 class="badge badge-default">OS SEGUINTES CAIXAS NÃO PODEM TER SALDO NEGATIVO:</h3><ul>${saldosNegativos.map(caixa => `<li>${caixa}</li>`).join('')}</ul></li>`;
+        }
+
+        $('#alertMessages').html(`<ul>${alertList}</ul>`);
+        $('#alertModal').modal('show');
+        $('.btn-confirm').prop('disabled', true);
+    } else {
+        $('#alertMessages').html('');
+        $('.btn-confirm').prop('disabled', false);
+    }
+
+    const formId = $('.btn-confirm').data('form-id');
+
+    $('.btn-confirm').on('click', function() {
+        swal({
+            title: 'Atenção, deseja realmente consolidar?',
+            text: 'Por favor, corrija os problemas listados antes de consolidar.',
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Sim',
+            confirmButtonColor: "#d33",
+            cancelButtonText: "Cancelar",
+            cancelButtonColor: "#3085d6",
+            padding: '2em'
+        }).then(function(result) {
+            if (result.value) {
+                document.getElementById(formId).submit();
             }
         });
     });
+});
+
 </script>
 
 @endsection
