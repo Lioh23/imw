4f01c4bd8a2ee30f791b1efec78af66f8c0b537b
Prof Vinicius <gvweb.solucoes@gmail.com>
2024-06-14 02:05:11 -0300

    Atualização Celular

--- resources/views/financeiro/consolidarcaixa.blade.php
+++ resources/views/financeiro/consolidarcaixa.blade.php
@@ -37,27 +37,6 @@
 
 @section('content')
 @include('extras.alerts')
-<!-- Modal -->
-<div class="modal fade" id="alertModal" tabindex="-1" role="dialog" aria-labelledby="alertModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document"> <!-- Use modal-xl para um modal ainda maior -->
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="alertModalLabel">Atenção!</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body" style="max-height: 400px; overflow-y: auto;">
-                <ul id="alertMessages">
-                    <!-- Alert messages will be displayed here -->
-                </ul>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
-            </div>
-        </div>
-    </div>
-</div>
 
 <div class="container-fluid">
 
@@ -189,7 +168,7 @@
                         </div>
                     </div>
                     <div class="col-12 text-center mt-3">
-                        <span id="warningMessage" class="alert alert-danger" style="display: none;"></span>
+                       <div id="alertContainer" class="mt-3" style="text-align: left;"></div>
 
                         <form class="mt-4" method="post" action="{{ route('financeiro.consolidar.store') }}" id="form_consolidacao_automatica">
                             @csrf
@@ -197,6 +176,7 @@
                             <input type="hidden" name="mes" value="{{ \Carbon\Carbon::parse($ultimoCaixa)->addMonth()->month }}" />
                             <button data-form-id="form_consolidacao_automatica" class="btn btn-success p-2 btn-rounded btn-confirm" style="text-transform: uppercase;" type="button">CONSOLIDAR {{ \Carbon\Carbon::parse($ultimoCaixa)->addMonth()->isoFormat('MMMM [de] YYYY') }}</button>
                         </form>
+
 
                     </div>
 
@@ -207,88 +187,71 @@
     </div>
 </div>
 <script>
-$(document).ready(function() {
-    // Adiciona badges conforme o saldo atual
-    $('.saldo-atual').each(function() {
-        var saldoText = $(this).text().replace('R$ ', '').replace('.', '').replace(',', '.');
-        var saldo = parseFloat(saldoText);
+    $(document).ready(function() {
+        // Verificações das regras
+        let shouldSubmit = true;
+        let totalEntradasZero = false;
+        let saldosNegativos = [];
 
-        if (saldo < 0) {
-            $(this).html('<span class="badge badge-danger">' + $(this).html() + '</span>');
-        } else if (saldo === 0) {
-            $(this).html('<span class="badge badge-secondary">' + $(this).html() + '</span>');
-        } else {
-            $(this).html('<span class="badge badge-success">' + $(this).html() + '</span>');
-        }
-    });
+        // Verifica o total de entradas de todos os caixas
+        let totalEntradasGeralText = $('tr:contains("Total dos Caixas")').find('td:eq(2)').text().replace('R$ ', '').replace('.', '').replace(',', '.');
+        let totalEntradasGeral = parseFloat(totalEntradasGeralText);
 
-    // Verificações das regras
-    let shouldSubmit = true;
-    let totalEntradasZero = false;
-    let saldosNegativos = [];
-
-    // Verifica o total de entradas de todos os caixas
-    let totalEntradasGeralText = $('tr:contains("Total dos Caixas")').find('td:eq(2)').text().replace('R$ ', '').replace('.', '').replace(',', '.');
-    let totalEntradasGeral = parseFloat(totalEntradasGeralText);
-
-    if (totalEntradasGeral === 0) {
-        totalEntradasZero = true;
-        shouldSubmit = false;
-    }
-
-    // Verifica saldo atual
-    $('.saldo-atual').each(function() {
-        var saldoText = $(this).text().replace('R$ ', '').replace('.', '').replace(',', '.');
-        var saldo = parseFloat(saldoText);
-        var caixa = $(this).closest('tr').find('td:first').text();
-
-        if (saldo < 0) {
-            saldosNegativos.push(caixa);
+        if (totalEntradasGeral === 0) {
+            totalEntradasZero = true;
             shouldSubmit = false;
         }
-    });
 
-    if (totalEntradasZero || saldosNegativos.length > 0) {
-        let alertList = '';
+        // Verifica saldo atual
+        $('.saldo-atual').each(function() {
+            var saldoText = $(this).text().replace('R$ ', '').replace('.', '').replace(',', '.');
+            var saldo = parseFloat(saldoText);
+            var caixa = $(this).closest('tr').find('td:first').text();
 
-        if (totalEntradasZero) {
-            alertList += `<li><h3 class="badge badge-danger">TOTAL DE ENTRADAS DOS CAIXAS É ZERO</h3></li>`;
-        }
-
-        if (saldosNegativos.length > 0) {
-            alertList += `<li><h3 class="badge badge-danger">OS SEGUINTES CAIXAS NÃO PODEM TER SALDO NEGATIVO: </h3><ul>${saldosNegativos.map(caixa => `<li>${caixa}</li>`).join('')}</ul></li>`;
-        }
-
-        $('#alertMessages').html(`<ul>${alertList}</ul>`);
-        $('#alertModal').modal('show');
-        $('.btn-confirm').prop('disabled', true);
-    } else {
-        $('#alertMessages').html('');
-        $('.btn-confirm').prop('disabled', false);
-    }
-
-    const formId = $('.btn-confirm').data('form-id');
-
-    $('.btn-confirm').on('click', function() {
-        swal({
-            title: 'Atenção!',
-            text: 'Deseja realmente consolidar? Esta ação é irreversível.',
-            type: 'warning',
-            showCancelButton: true,
-            confirmButtonText: 'Sim',
-            confirmButtonColor: "#d33",
-            cancelButtonText: "Cancelar",
-            cancelButtonColor: "#3085d6",
-            padding: '2em'
-        }).then(function(result) {
-            if (result.value) {
-                document.getElementById(formId).submit();
+            if (saldo < 0) {
+                saldosNegativos.push(caixa);
+                shouldSubmit = false;
             }
         });
+
+        if (totalEntradasZero || saldosNegativos.length > 0) {
+            let alertList = '';
+
+            if (totalEntradasZero) {
+                alertList += `<div class="alert alert-danger" role="alert">TOTAL DE ENTRADAS DOS CAIXAS É ZERO</div>`;
+            }
+
+            if (saldosNegativos.length > 0) {
+                alertList += `<div class="alert alert-danger" role="alert">OS SEGUINTES CAIXAS NÃO PODEM TER SALDO NEGATIVO: <ul>${saldosNegativos.map(caixa => `<li>${caixa}</li>`).join('')}</ul></div>`;
+            }
+
+            $('#alertContainer').html(alertList);
+            $('.btn-confirm').prop('disabled', true);
+        } else {
+            $('#alertContainer').html('');
+            $('.btn-confirm').prop('disabled', false);
+        }
+
+        const formId = $('.btn-confirm').data('form-id');
+
+        $('.btn-confirm').on('click', function() {
+            swal({
+                title: 'Atenção!',
+                text: 'Deseja realmente consolidar? Esta ação é irreversível.',
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonText: 'Sim',
+                confirmButtonColor: "#d33",
+                cancelButtonText: "Cancelar",
+                cancelButtonColor: "#3085d6",
+                padding: '2em'
+            }).then(function(result) {
+                if (result.value) {
+                    document.getElementById(formId).submit();
+                }
+            });
+        });
     });
-});
-
-
 </script>
 
 @endsection
