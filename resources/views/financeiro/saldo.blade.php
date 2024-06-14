7c6d7b76b8f2e7a2139bf164283fc481cafee770
Prof Vinicius <gvweb.solucoes@gmail.com>
2024-06-13 21:47:55 -0300

    SALDO ATUAL

--- resources/views/financeiro/saldo.blade.php
+++ resources/views/financeiro/saldo.blade.php
@@ -1,101 +1,118 @@
 @extends('template.layout')
 @section('breadcrumb')
-    <x-breadcrumb :breadcrumbs="[
+<x-breadcrumb :breadcrumbs="[
         ['text' => 'Home', 'url' => '/', 'active' => false],
         ['text' => 'Movimento de Caixa', 'url' => '/financeiro/movimento-caixa', 'active' => false],
         ['text' => 'Saldo', 'url' => '#', 'active' => true],
     ]"></x-breadcrumb>
 @endsection
 @section('content')
-    <div class="container-fluid">
+<div class="container-fluid">
 
-    </div>
-    <div class="col-lg-12 col-12 layout-spacing">
-        <div class="statbox widget box box-shadow">
-            <div class="widget-content widget-content-area">
-                <!-- Conteúdo -->
-                <div class="card mb-3">
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-12">
-                                <h5 class="mt-3">Saldo Atual</h5>
-                                <table class="table table-striped" style="font-size: 90%; margin-top: 15px;">
-                                    <thead class="thead-dark">
-                                        <tr>
-                                            <th>CAIXA</th>
-                                            <th width="300" style="text-align: right">
-                                                @if ($ultimoCaixa)
-                                                    ÚLTIMO SALDO CONSOLIDADO EM 
-                                                    {{ \Carbon\Carbon::parse($ultimoCaixa)->isoFormat('MMMM [de] YYYY') }}
-                                                @else
-                                                    NÃO POSSUI ÚLTIMO SALDO CONSOLIDADO
-                                                @endif
-                                            </th>
-                                            <th width="100" style="text-align: right">TOTAIS DE ENTRADAS</th>
-                                            <th width="100" style="text-align: right">TOTAIS DE SAÍDAS</th>
-                                            <th width="100" style="text-align: right">TRANSF. ENTRADAS</th>
-                                            <th width="100" style="text-align: right">TRANSF. SAÍDAS</th>
-                                            <th width="100" style="text-align: right">SALDO ATUAL</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        @php
-                                            $totalUltimoConciliado = 0;
-                                            $totalEntradas = 0;
-                                            $totalSaidas = 0;
-                                            $totalTransfEntradas = 0;
-                                            $totalTransfSaidas = 0;
-                                            $saldoAtualNaoConciliado = 0;
-                                        @endphp
-                                
-                                        @foreach ($caixas as $caixa)
-                                            @php
-                                                $totalUltimoConciliado += $caixa->totalLancamentosUltimosConciliados();
-                                                $totalEntradas += $caixa->totalLancamentosNaoConciliadosEntrada();
-                                                $totalSaidas += $caixa->totalLancamentosNaoConciliadosSaida();
-                                                $totalTransfEntradas += $caixa->totalLancamentosNaoConciliadosTransferenciaEntrada();
-                                                $totalTransfSaidas += $caixa->totalLancamentosNaoConciliadosTransferenciaSaida();
-                                                $saldoAtualNaoConciliado += $caixa->saldoAtualNaoConciliado();
-                                            @endphp
-                                            <tr>
-                                                <td style="text-align: left">{{ $caixa->descricao }}</td>
-                                                <td style="text-align: right">R${{ number_format($caixa->totalLancamentosUltimosConciliados(), 2, ',', '.') }}</td>
-                                                <td style="text-align: right">R${{ number_format($caixa->totalLancamentosNaoConciliadosEntrada(), 2, ',', '.') }}</td>
-                                                <td style="text-align: right">{!! $caixa->totalLancamentosNaoConciliadosSaida() > 0 ? '<span>-R$' . number_format($caixa->totalLancamentosNaoConciliadosSaida(), 2, ',', '.') . '</span>' : 'R$' . number_format($caixa->totalLancamentosNaoConciliadosSaida(), 2, ',', '.') !!}</td>
-                                                <td style="text-align: right">R${{ number_format($caixa->totalLancamentosNaoConciliadosTransferenciaEntrada(), 2, ',', '.') }}</td>
-                                                <td style="text-align: right">{!! $caixa->totalLancamentosNaoConciliadosTransferenciaSaida() != 0 ? '<span>-R$' . number_format(abs($caixa->totalLancamentosNaoConciliadosTransferenciaSaida()), 2, ',', '.') . '</span>' : 'R$' . number_format(abs($caixa->totalLancamentosNaoConciliadosTransferenciaSaida()), 2, ',', '.') !!}</td>
-                                                <td style="text-align: right">{!! $caixa->saldoAtualNaoConciliado() > 0 ? '<span class="badge badge-success">R$' . number_format($caixa->saldoAtualNaoConciliado(), 2, ',', '.') . '</span>' : ($caixa->saldoAtualNaoConciliado() < 0 ? '<span class="badge badge-danger">-R$' . number_format($caixa->saldoAtualNaoConciliado(), 2, ',', '.') . '</span>' : ($caixa->saldoAtualNaoConciliado() == 0 ? '<span class="badge badge-secondary">R$' . number_format($caixa->saldoAtualNaoConciliado(), 2, ',', '.') . '</span>' : 'R$' . number_format($caixa->saldoAtualNaoConciliado(), 2, ',', '.'))) !!}</td>
-                                            </tr>
-                                        @endforeach
-                                
-                                        <tr>
-                                            <td style="text-align: left"><strong><b>Total dos Caixas</b></strong></td>
-                                            <td style="text-align: right"><strong>R${{ number_format($totalUltimoConciliado, 2, ',', '.') }}</strong></td>
-                                            <td style="text-align: right"><strong>R${{ number_format($totalEntradas, 2, ',', '.') }}</strong></td>
-                                            <td style="text-align: right"><strong>{!! $totalSaidas > 0 ? '<span>-R$' . number_format($totalSaidas, 2, ',', '.') . '</span>' : 'R$' . number_format($totalSaidas, 2, ',', '.') !!}</strong></td>
-                                            <td style="text-align: right"><strong>R${{ number_format($totalTransfEntradas, 2, ',', '.') }}</strong></td>
-                                            <td style="text-align: right"><strong>{!! $totalTransfSaidas > 0 ? '<span>-R$' . number_format($totalTransfSaidas, 2, ',', '.') . '</span>' : 'R$' . number_format($totalTransfSaidas, 2, ',', '.') !!}</strong></td>
-                                            <td style="text-align: right">{!! $saldoAtualNaoConciliado > 0 ? '<strong><span class="badge badge-success">R$' . number_format($saldoAtualNaoConciliado, 2, ',', '.') . '</span></strong>' : ($saldoAtualNaoConciliado < 0 ? '<strong><span class="badge badge-danger">-R$' . number_format($saldoAtualNaoConciliado, 2, ',', '.') . '</span></strong>' : ($saldoAtualNaoConciliado == 0 ? '<strong><span class="badge badge-secondary">R$' . number_format($saldoAtualNaoConciliado, 2, ',', '.') . '</span></strong>' : number_format($saldoAtualNaoConciliado, 2, ',', '.'))) !!}</td>
-                                        </tr>
-                                    </tbody>
-                                </table>                                
-                            </div>
+</div>
+<div class="col-lg-12 col-12 layout-spacing">
+    <div class="statbox widget box box-shadow">
+        <div class="widget-content widget-content-area">
+            <!-- Conteúdo -->
+            <div class="card mb-3">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-12">
+                            <h5 class="mt-3">SALDO {{ strtoupper(\Carbon\Carbon::parse($ultimoCaixa)->addMonth()->isoFormat('MMMM [de] YYYY')) }}</h5>
+                            <table class="table table-striped" style="font-size: 90%; margin-top: 15px;">
+                                <thead class="thead-dark">
+                                    <tr>
+                                        <th>CAIXA</th>
+                                        <th width="300" style="text-align: right">ÚLTIMO SALDO CONSOLIDADO EM {{ \Carbon\Carbon::parse($ultimoCaixa)->isoFormat('MMMM [de] YYYY') }}</th>
+                                        <th width="120" style="text-align: right">TOTAIS DE ENTRADAS</th>
+                                        <th width="120" style="text-align: right">TOTAIS DE SAÍDAS</th>
+                                        <th width="120" style="text-align: right">TRANSF. ENTRADAS</th>
+                                        <th width="120" style="text-align: right">TRANSF. SAÍDAS</th>
+                                        <th width="120" style="text-align: right">SALDO ATUAL</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @php
+                                    $totalSaldoFinal = 0;
+                                    $totalEntradas = 0;
+                                    $totalSaidas = 0;
+                                    $totalTransferenciasEntrada = 0;
+                                    $totalTransferenciasSaida = 0;
+                                    $totalSaldoAtual = 0;
+                                    @endphp
+
+                                    @foreach ($caixas as $caixa)
+                                    <tr>
+                                        <td style="text-align: left">{{ $caixa->caixa }}</td>
+                                        <td style="text-align: right">
+                                            {{ 'R$ ' . number_format($caixa->saldo_final, 2, ',', '.') }}
+                                        </td>
+                                        <td style="text-align: right">
+                                            {{ 'R$ ' . number_format($caixa->total_entradas, 2, ',', '.') }}
+                                        </td>
+                                        <td style="text-align: right">
+                                            {{ 'R$ ' . ($caixa->total_saidas > 0 ? '-' : '') . number_format(abs($caixa->total_saidas), 2, ',', '.') }}
+                                        </td>
+                                        <td style="text-align: right">
+                                            {{ 'R$ ' . number_format($caixa->total_transferencias_entrada, 2, ',', '.') }}
+                                        </td>
+                                        <td style="text-align: right">
+                                            {{ 'R$ ' . ($caixa->total_transferencias_saida > 0 ? '-' : '') . number_format(abs($caixa->total_transferencias_saida), 2, ',', '.') }}
+                                        </td>
+                                        <td style="text-align: right">
+                                            {{ 'R$ ' . number_format($caixa->saldo_atual, 2, ',', '.') }}
+                                        </td>
+                                    </tr>
+                                    @php
+                                    $totalSaldoFinal += $caixa->saldo_final;
+                                    $totalEntradas += $caixa->total_entradas;
+                                    $totalSaidas += $caixa->total_saidas;
+                                    $totalTransferenciasEntrada += $caixa->total_transferencias_entrada;
+                                    $totalTransferenciasSaida += $caixa->total_transferencias_saida;
+                                    $totalSaldoAtual += $caixa->saldo_atual;
+                                    @endphp
+                                    @endforeach
+
+                                    {{-- Total de cada caixa --}}
+                                    <tr>
+                                        <td style="text-align: left"><strong>Total dos Caixas</strong></td>
+                                        <td style="text-align: right">
+                                            <strong>{{ 'R$ ' . number_format($totalSaldoFinal, 2, ',', '.') }}</strong>
+                                        </td>
+                                        <td style="text-align: right">
+                                            <strong>{{ 'R$ ' . number_format($totalEntradas, 2, ',', '.') }}</strong>
+                                        </td>
+                                        <td style="text-align: right">
+                                            <strong>{{ 'R$ ' . ($totalSaidas > 0 ? '-' : '') . number_format(abs($totalSaidas), 2, ',', '.') }}</strong>
+                                        </td>
+                                        <td style="text-align: right">
+                                            <strong>{{ 'R$ ' . number_format($totalTransferenciasEntrada, 2, ',', '.') }}</strong>
+                                        </td>
+                                        <td style="text-align: right">
+                                            <strong>{{ 'R$ ' . ($totalTransferenciasSaida > 0 ? '-' : '') . number_format(abs($totalTransferenciasSaida), 2, ',', '.') }}</strong>
+                                        </td>
+                                        <td style="text-align: right">
+                                            <strong>{{ 'R$ ' . number_format($totalSaldoAtual, 2, ',', '.') }}</strong>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
-                        <div class="row">
-                            <div class="col-12 text-center">
-                                <button class="btn btn-success btn-rounded" onclick="exportReportToExcel();"><i
-                                        class="fa fa-file-excel" aria-hidden="true"></i> Exportar</button>
-                            </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 text-center">
+                            <button class="btn btn-success btn-rounded" onclick="exportReportToExcel();"><i class="fa fa-file-excel" aria-hidden="true"></i> Exportar</button>
                         </div>
                     </div>
                 </div>
-                <!-- Fim do Conteúdo -->
             </div>
+            <!-- Fim do Conteúdo -->
         </div>
     </div>
-    <script src="{{asset('theme/assets/js/planilha/papaparse.min.js')}}"></script>
-    <script src="{{asset('theme/assets/js/planilha/FileSaver.min.js')}}"></script>
-    <script src="{{asset('theme/assets/js/planilha/xlsx.full.min.js')}}"></script>
-    <script src="{{asset('theme/assets/js/planilha/planilha.js')}}"></script>
-    <script src="{{asset('theme/assets/js/pages/movimentocaixa.js')}}"></script>
+</div>
+<script src="{{asset('theme/assets/js/planilha/papaparse.min.js')}}"></script>
+<script src="{{asset('theme/assets/js/planilha/FileSaver.min.js')}}"></script>
+<script src="{{asset('theme/assets/js/planilha/xlsx.full.min.js')}}"></script>
+<script src="{{asset('theme/assets/js/planilha/planilha.js')}}"></script>
+<script src="{{asset('theme/assets/js/pages/movimentocaixa.js')}}"></script>
 @endsection
