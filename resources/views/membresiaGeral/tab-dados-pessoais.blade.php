432a8544d1c54a69536c904266527067fc6a6624
Rallenson <Rallenson90@gmail.com>
2024-06-16 11:16:36 -0300

    feat: add date of birth input field to member profile editing page

--- resources/views/membresiaGeral/tab-dados-pessoais.blade.php
+++ resources/views/membresiaGeral/tab-dados-pessoais.blade.php
@@ -1,3 +1,10 @@
-  <div class="tab-pane fade show active" id="border-top-dados-pessoal" role="tabpanel" aria-labelledby="border-top-dados-pessoais">
-    Dados Pessoais
-  </div>
+<div class=" card tab-pane fade show active" id="border-top-dados-pessoal" role="tabpanel"
+    aria-labelledby="border-top-dados-pessoais">
+    <div class="card-body">
+        <h5 class="card-title">{{ $membro['nome'] }}</h5>
+        <div class="card mb-3">
+            <input type="date" class="card-text" name="data_nascimento" value="{{ old('data_nascimento', optional($membro['data_nascimento'])->format('Y-m-d')) }}" style="border: none; text-align: left; background: tranparent" readonly>
+        </div>
+    </div>
+</div>
+
--- resources/views/membros/editar/tab-dados-pessoais.blade.php
+++ resources/views/membros/editar/tab-dados-pessoais.blade.php
@@ -26,7 +26,7 @@
 
               <div class="col-xl-3">
                 <label for="nascimento">* Data de Nascimento</label>
-                <input type="date" class="form-control @error('data_nascimento') is-invalid @enderror" id="data_nascimento" name="data_nascimento" value="{{ old('data_nascimento', optional($pessoa->data_nascimento)->format('Y-m-d')) }}" min="1900-01-01">
+                  
                 @error('data_nascimento')
                 <span class="help-block text-danger">{{ $message }}</span>
                 @enderror
