65327f099f0f00c6fb9ff47b0b73913601868fce
Prof Vinicius <gvweb.solucoes@gmail.com>
2024-06-21 17:25:54 -0300

    Bug Entrada

--- resources/views/financeiro/movimentocaixa.blade.php
+++ resources/views/financeiro/movimentocaixa.blade.php
@@ -55,7 +55,6 @@
     <script src="{{ asset('theme/plugins/sweetalerts/promise-polyfill.js') }}"></script>
     <script src="{{ asset('theme/plugins/sweetalerts/sweetalert2.min.js') }}"></script>
     <script src="{{ asset('theme/plugins/sweetalerts/sweetalert2.min.js') }}"></script>
-    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
