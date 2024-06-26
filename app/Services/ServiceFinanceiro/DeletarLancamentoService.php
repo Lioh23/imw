b04e8fac6d8ee3ba58ce3393fc9de829fd12f96c
Prof Vinicius <gvweb.solucoes@gmail.com>
2024-06-26 00:04:02 -0300

    Excluir Lancamento

--- app/Services/ServiceFinanceiro/DeletarLancamentoService.php
+++ app/Services/ServiceFinanceiro/DeletarLancamentoService.php
@@ -13,7 +13,6 @@ class DeletarLancamentoService
     {
         $lancamento = FinanceiroLancamento::findOrFail($id);
         if ($lancamento->membro_id) {
-            dd($lancamento);
             $planoContaIds = [3, 4, 5, 6, 110172, 110173, 110174, 110186];
             if ($lancamento->membro_id && in_array($lancamento->plano_conta_id, $planoContaIds)) {
                 $this->handleLivroGrade($lancamento->membro_id, $lancamento->valor, $lancamento->data_movimento, $lancamento->id);
@@ -96,6 +95,8 @@ class DeletarLancamentoService
                     ->orWhere($mes, '!=', '0');
             })
             ->first();
+
+        dd($existingLancamentoOld);
 
         $total = $existingLancamentoOld->$mes - $data['valor'];
        
