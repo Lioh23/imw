de447204780636b462d640c85fd5fb0220ee5169
Caião <caiobges@hotmail.com>
2024-06-25 23:14:54 -0300

    fix:autocomplete nos campos do endereço quando coloca o CEP

--- resources/views/congregacoes/edit.blade.php
+++ resources/views/congregacoes/edit.blade.php
@@ -176,8 +176,6 @@
 
 @endsection
 @section('extras-scripts')
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
     <script>
         $(document).ready(function() {
             // Aplica a máscara ao campo CEP
