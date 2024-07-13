ca2e8bc42c72ce1d89b59b2cfb9b2bb9757199c8
Caião <caiobges@hotmail.com>
2024-07-12 22:44:19 -0300

    fix:mascara no input

--- resources/views/admin/editar.blade.php
+++ resources/views/admin/editar.blade.php
@@ -345,4 +345,10 @@
         });
     });
 </script>
+<script>
+    $(document).ready(function() {
+        $('#telefone').mask('(00) 00000-0000');
+        $('#cpf').mask('000.000.000-00');
+    });
+</script>
 @endsection
