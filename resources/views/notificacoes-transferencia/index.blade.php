7b7a79af816fac3788cb0b995e5371179669ec3c
Rallenson <Rallenson90@gmail.com>
2024-07-14 11:27:21 -0300

    refactyor: modificar para que as notificações só aparecam quando ainda não foi tomaod uma ação quanto a tranferência

--- resources/views/notificacoes-transferencia/index.blade.php
+++ resources/views/notificacoes-transferencia/index.blade.php
@@ -26,45 +26,45 @@
 
     @if ($baseParams->notificacoesTransferencia && $baseParams->notificacoesTransferencia->count())
         @foreach ($baseParams->notificacoesTransferencia as $notificacao)
-            <a class="media server-log"
-                href="{{ route('membro.receber_membro_externo', ['notificacao' => $notificacao->id]) }}">
-                <div class="col-md-12" style="margin: 16px;">
-                    <div id='left-rollbacks' class='dragula'>
-                        <div class="card post text-post" style="">
-                            <div class="card-body">
-                                <div class="media user-meta d-sm-flex d-block text-sm-left text-center">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="64" height="32"
-                                        fill="currentColor" class="bi bi-door-open" viewBox="0 0 16 16">
-                                        <path d="M8.5 10c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1" />
-                                        <path
-                                            d="M10.828.122A.5.5 0 0 1 11 .5V1h.5A1.5 1.5 0 0 1 13 2.5V15h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V1.5a.5.5 0 0 1 .43-.495l7-1a.5.5 0 0 1 .398.117M11.5 2H11v13h1V2.5a.5.5 0 0 0-.5-.5M4 1.934V15h6V1.077z" />
-                                    </svg>
-                                    <div class="media-body">
-                                        <h5 class="">{{ $notificacao->membro->nome }}</h5>
-                                        @php
-                                            $dtAbertura = $notificacao->dt_abertura
-                                                ? \Carbon\Carbon::parse($notificacao->dt_abertura)
-                                                : null;
-                                            $format = $dtAbertura && $dtAbertura->diffInHours() < 48 ? 'H:i' : 'd/m/Y';
-
-                                        @endphp
-                                        <p class="meta-time">
-                                            {{ $dtAbertura ? $dtAbertura->format($format) : 'Sem informações' }}
-                                        </p>
+            @if (!$notificacao->dt_aceite && !$notificacao->dt_rejeicao)
+                <a class="media server-log"
+                    href="{{ route('membro.receber_membro_externo', ['notificacao' => $notificacao->id]) }}">
+                    <div class="col-md-12" style="margin: 16px;">
+                        <div id='left-rollbacks' class='dragula'>
+                            <div class="card post text-post" style="">
+                                <div class="card-body">
+                                    <div class="media user-meta d-sm-flex d-block text-sm-left text-center">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="64" height="32"
+                                            fill="currentColor" class="bi bi-door-open" viewBox="0 0 16 16">
+                                            <path d="M8.5 10c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1" />
+                                            <path
+                                                d="M10.828.122A.5.5 0 0 1 11 .5V1h.5A1.5 1.5 0 0 1 13 2.5V15h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V1.5a.5.5 0 0 1 .43-.495l7-1a.5.5 0 0 1 .398.117M11.5 2H11v13h1V2.5a.5.5 0 0 0-.5-.5M4 1.934V15h6V1.077z" />
+                                        </svg>
+                                        <div class="media-body">
+                                            <h5 class="">{{ $notificacao->membro->nome }}</h5>
+                                            @php
+                                                $dtAbertura = $notificacao->dt_abertura
+                                                    ? \Carbon\Carbon::parse($notificacao->dt_abertura)
+                                                    : null;
+                                                $format =
+                                                    $dtAbertura && $dtAbertura->diffInHours() < 48 ? 'H:i' : 'd/m/Y';
+                                            @endphp
+                                            <p class="meta-time">
+                                                {{ $dtAbertura ? $dtAbertura->format($format) : 'Sem informações' }}
+                                            </p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="post-content  text-sm-left text-center" style="padding: 16px;">
-                                <h6 class="badge badge-success col-md-12" style="height: 64; font-size: 1.2rem;">Nova
-                                    transferência para
-                                    esta instituição</h6>
+                                <div class="post-content  text-sm-left text-center" style="padding: 16px;">
+                                    <h6 class="badge badge-success col-md-12" style="height: 64; font-size: 1.2rem;">Nova
+                                        transferência para
+                                        esta instituição</h6>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                </div>
-                </div>
-            </a>
+                </a>
+            @endif
         @endforeach
     @endif
 @endsection
