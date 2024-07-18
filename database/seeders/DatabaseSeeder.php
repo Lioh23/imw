a6dfbc405c6127f3090dae8621aa1c89e15a72df
Prof Vinicius <gvweb.solucoes@gmail.com>
2024-07-18 01:07:14 -0300

    Financeiro Distrito

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -31,5 +31,6 @@ class DatabaseSeeder extends Seeder
         $this->call(RegrasExtrasTableSeeder::class);
         $this->call(PerfilRegraTableSeeder::class);
         $this->call(UserSeeder::class);
+        $this->call(FinanceiroPlanoContaTipoInstituicaoSeeder::class);
     }
 }
