ad81b055656cbaca5dd0542ba7188fd86f364a6e
Prof Vinicius <gvweb.solucoes@gmail.com>
2024-07-26 19:27:37 -0300

    Regras Menu

--- resources/views/template/menu-sidebar.blade.php
+++ resources/views/template/menu-sidebar.blade.php
@@ -208,10 +208,7 @@
              <li class="menu">
                  <a href="#financeiro-distrito" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
                      <div class="">
-                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-dollar-sign">
-                             <line x1="12" y1="1" x2="12" y2="23"></line>
-                             <path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path>
-                         </svg>
+                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
                          <span>Relatório Distrito</span>
                      </div>
                      <div>
@@ -228,17 +225,17 @@
                      </li>
                      <li>
                          @if (auth()->check() && auth()->user()->hasPerfilRegra('distrito-relatorio-orcamento'))
-                         <a href="{{ route('distrito.relatorio.orcamento') }}">Livro Razão Geral</a>
+                         <a href="{{ route('distrito.relatorio.orcamento') }}">Orçamento</a>
                          @endif
                      </li>
                      <li>
                          @if (auth()->check() && auth()->user()->hasPerfilRegra('distrito-relatorio-variacao-financeira'))
-                         <a href="{{ route('distrito.relatorio.variacaofinanceira') }}">Livro Razão Geral</a>
+                         <a href="{{ route('distrito.relatorio.variacaofinanceira') }}">Variação Financeira</a>
                          @endif
                      </li>
                      <li>
                          @if (auth()->check() && auth()->user()->hasPerfilRegra('distrito-relatorio-lancamento-das-igrejas'))
-                         <a href="{{ route('distrito.relatorio.lancamentodasigrejas') }}">Livro Razão Geral</a>
+                         <a href="{{ route('distrito.relatorio.lancamentodasigrejas') }}">Lançamento das Igrejas</a>
                          @endif
                      </li>
                  </ul>
