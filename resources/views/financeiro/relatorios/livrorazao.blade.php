417d7e389b2225f0cc2e84467ffeb553cac76601
Prof Vinicius <gvweb.solucoes@gmail.com>
2024-07-27 03:43:13 -0300

    Livro Razao

--- resources/views/financeiro/relatorios/livrorazao.blade.php
+++ resources/views/financeiro/relatorios/livrorazao.blade.php
@@ -1,7 +1,7 @@
 @extends('template.layout')
 
 @section('breadcrumb')
-<x-breadcrumb :breadcrumbs="[
+    <x-breadcrumb :breadcrumbs="[
         ['text' => 'Home', 'url' => '/', 'active' => false],
         ['text' => 'Relatórios - Financeiro', 'url' => '#', 'active' => false],
         ['text' => 'Livro Razão', 'url' => '#', 'active' => true],
@@ -9,168 +9,168 @@
 @endsection
 
 @section('extras-css')
-<link href="{{ asset('theme/assets/css/elements/alert.css') }}" rel="stylesheet" type="text/css" />
-<link href="{{ asset('theme/assets/css/forms/theme-checkbox-radio.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('theme/assets/css/elements/alert.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('theme/assets/css/forms/theme-checkbox-radio.css') }}" rel="stylesheet" type="text/css" />
 @endsection
 
 @include('extras.alerts')
 
 @section('content')
-<div class="col-lg-12 col-12 layout-spacing">
-    <div class="statbox widget box box-shadow">
-        <div class="widget-header">
-            <div class="row">
-                <div class="col-xl-12 col-md-12 col-sm-12 col-12">
-                    <h4>Relatório Livro Razão</h4>
+    <div class="col-lg-12 col-12 layout-spacing">
+        <div class="statbox widget box box-shadow">
+            <div class="widget-header">
+                <div class="row">
+                    <div class="col-xl-12 col-md-12 col-sm-12 col-12">
+                        <h4>Relatório Livro Razão</h4>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="widget-content widget-content-area">
-            <form class="form-vertical" id="filter_form" method="GET">
-                <div class="form-group row mb-4" id="filtros_data">
-                    <div class="col-lg-2 text-right">
-                        <label class="control-label">* Data Inicial:</label>
-                    </div>
-                    <div class="col-lg-3">
-                        <input type="date" class="form-control @error('dt_inicial') is-invalid @enderror" id="dt_inicial" name="dt_inicial" value="{{ request()->input('dt_inicial') }}" required>
-                    </div>
+            <div class="widget-content widget-content-area">
+                <form class="form-vertical" id="filter_form" method="GET">
+                    <div class="form-group row mb-4" id="filtros_data">
+                        <div class="col-lg-2 text-right">
+                            <label class="control-label">* Data Inicial:</label>
+                        </div>
+                        <div class="col-lg-3">
+                            <input type="date" class="form-control @error('dt_inicial') is-invalid @enderror" id="dt_inicial" name="dt_inicial" value="{{ request()->input('dt_inicial') }}" required>
+                        </div>
 
-                    <div class="col-lg-2 text-right">
-                        <label class="control-label">* Data Final:</label>
+                        <div class="col-lg-2 text-right">
+                            <label class="control-label">* Data Final:</label>
+                        </div>
+                        <div class="col-lg-3">
+                            <input type="date" class="form-control @error('dt_final') is-invalid @enderror" id="dt_final" name="dt_final" value="{{ request()->input('dt_final') }}" required>
+                        </div>
                     </div>
-                    <div class="col-lg-3">
-                        <input type="date" class="form-control @error('dt_final') is-invalid @enderror" id="dt_final" name="dt_final" value="{{ request()->input('dt_final') }}" required>
+                    <div class="form-group row mb-4">
+                        <div class="col-lg-2"></div>
+                        <div class="col-lg-6">
+                            <button id="btn_buscar" type="submit" name="action" value="buscar" title="Buscar dados do Relatório" class="btn btn-primary btn">
+                                <x-bx-search /> Buscar
+                            </button>
+                            <button id="btn_relatorio" type="button" class="btn btn-secondary">
+                                <i class="fa fa-file-pdf"></i> Relatório
+                            </button>
+                        </div>
                     </div>
-                </div>
-                <div class="form-group row mb-4">
-                    <div class="col-lg-2"></div>
-                    <div class="col-lg-6">
-                        <button id="btn_buscar" type="submit" name="action" value="buscar" title="Buscar dados do Relatório" class="btn btn-primary btn">
-                            <x-bx-search /> Buscar
-                        </button>
-                        <button id="btn_relatorio" type="button" class="btn btn-secondary">
-                            <i class="fa fa-file-pdf"></i> Relatório
-                        </button>
-                    </div>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
-
-@if (!empty(request()->input('dt_inicial')))
-<div class="col-lg-12 col-12 layout-spacing">
-    <div class="statbox widget box box-shadow">
-        <div class="widget-header">
-            <div class="row">
-                <div class="col-xl-12 col-md-12 col-sm-12 col-12">
-                    <h4 style="text-transform: uppercase">Livro Caixa - {{ session()->get('session_perfil')->instituicoes->igrejaLocal->nome }}</h4>
-                    <p class="pl-3">Período de {{ \Carbon\Carbon::parse(request()->input('dt_inicial'))->format('d/m/Y') }} a {{ \Carbon\Carbon::parse(request()->input('dt_final'))->format('d/m/Y') }}</p>
-                </div>
-            </div>
-        </div>
-        <div class="widget-content widget-content-area">
-            <div class="table-responsive">
-                <table class="table table-striped" style="font-size: 90%; margin-top: 15px;">
-                    <thead>
-                        <tr>
-                            <th>CONTA/DATA/ORIGEM/DESTINO</th>
-                            <th></th>
-                            <th></th>
-                            <th>ENTRADA </th>
-                            <th class="text-right">SAÍDA</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($lancamentosPorConta as $planoConta)
-                        <tr style="font-weight: bold">
-                            <td colspan="4">{{ $planoConta->numeracao }} - {{ $planoConta->nome }}</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td>Total: {{ $planoConta->lancamentosPorIgreja->whereBetween('data_lancamento', [request()->input('dt_inicial'), request()->input('dt_final')])->sum('valor') }}</td>
-                        </tr>
-                        @php
-                        $totalEntradas = 0;
-                        $totalSaidas = 0;
-                        @endphp
-
-                        @foreach ($planoConta->lancamentosPorIgreja->whereBetween('data_lancamento', [request()->input('dt_inicial'), request()->input('dt_final')]) as $lancamento)
-
-                        <tr>
-                            <td colspan="3">
-                                {{ \Carbon\Carbon::parse($lancamento->data_lancamento)->format('d/m/Y') }} -
-                                {{ $lancamento->caixa->descricao }} -
-                                {{ $lancamento->pagante_favorecido }}
-                            </td>
-                            <td class="text-right {{ $lancamento->tipo_lancamento === 'E' ? 'green' : '' }}">
-                                {{ $lancamento->tipo_lancamento === 'E' ? 'R$ ' . number_format($lancamento->valor, 2, ',', '.') : '-' }}
-                            </td>
-                            <td class="text-right {{ $lancamento->tipo_lancamento === 'S' ? 'red' : '' }}">
-                                {{ $lancamento->tipo_lancamento === 'S' ? 'R$ ' . number_format($lancamento->valor, 2, ',', '.') : '-' }}
-                            </td>
-                        </tr>
-                        @php
-                        if ($lancamento->tipo_lancamento === 'E') {
-                        $totalEntradas += $lancamento->valor;
-                        } else {
-                        $totalSaidas += abs($lancamento->valor);
-                        }
-                        @endphp
-                        @endforeach
-                        <tr class="bold">
-                            {{-- <td>Total {{ $conta }}</td> --}}
-                            <td></td>
-                            <td></td>
-                            <td class="text-right">{{ number_format($totalEntradas, 2, ',', '.') }}</td>
-                            <td class="text-right">{{ number_format($totalSaidas, 2, ',', '.') }}</td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-12 text-center">
-                <button class="btn btn-success btn-rounded" onclick="exportReportToExcel();"><i class="fa fa-file-excel" aria-hidden="true"></i> Exportar</button>
+                </form>
             </div>
         </div>
     </div>
-    @endif
+
+    @if (!empty(request()->input('dt_inicial')))
+        <div class="col-lg-12 col-12 layout-spacing">
+            <div class="statbox widget box box-shadow">
+                <div class="widget-header">
+                    <div class="row">
+                        <div class="col-xl-12 col-md-12 col-sm-12 col-12">
+                            <h4 style="text-transform: uppercase">Livro Caixa - {{ session()->get('session_perfil')->instituicoes->igrejaLocal->nome }}</h4>
+                            <p class="pl-3">Período de {{ \Carbon\Carbon::parse(request()->input('dt_inicial'))->format('d/m/Y') }} a {{ \Carbon\Carbon::parse(request()->input('dt_final'))->format('d/m/Y') }}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="widget-content widget-content-area">
+                    <div class="table-responsive">
+                        <table>
+                            <thead>
+                            <tr>
+                                <th>CONTA/DATA/ORIGEM/DESTINO</th>
+                                <th></th>
+                                <th></th>
+                                <th>ENTRADA </th>
+                                <th class="text-right">SAÍDA</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach ($lancamentosPorConta as $planoConta)
+                                <tr style="font-weight: bold">
+                                    <td colspan="4">{{ $planoConta->numeracao }} - {{ $planoConta->nome }}</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td>Total: {{ $planoConta->lancamentosPorIgreja->whereBetween('data_lancamento', [request()->input('dt_inicial'), request()->input('dt_final')])->sum('valor') }}</td>
+                                </tr>
+                                @php
+                                    $totalEntradas = 0;
+                                    $totalSaidas = 0;
+                                @endphp
+
+                                @foreach ($planoConta->lancamentosPorIgreja->whereBetween('data_lancamento', [request()->input('dt_inicial'), request()->input('dt_final')]) as $lancamento)
+
+                                    <tr>
+                                        <td colspan="3">
+                                            {{ \Carbon\Carbon::parse($lancamento->data_lancamento)->format('d/m/Y') }} -
+                                            {{ $lancamento->caixa->descricao }} -
+                                            {{ $lancamento->pagante_favorecido }}
+                                        </td>
+                                        <td class="text-right {{ $lancamento->tipo_lancamento === 'E' ? 'green' : '' }}">
+                                            {{ $lancamento->tipo_lancamento === 'E' ? 'R$ ' . number_format($lancamento->valor, 2, ',', '.') : '-' }}
+                                        </td>
+                                        <td class="text-right {{ $lancamento->tipo_lancamento === 'S' ? 'red' : '' }}">
+                                            {{ $lancamento->tipo_lancamento === 'S' ? 'R$ ' . number_format($lancamento->valor, 2, ',', '.') : '-' }}
+                                        </td>
+                                    </tr>
+                                    @php
+                                        if ($lancamento->tipo_lancamento === 'E') {
+                                            $totalEntradas += $lancamento->valor;
+                                        } else {
+                                            $totalSaidas += abs($lancamento->valor);
+                                        }
+                                    @endphp
+                                @endforeach
+                                <tr class="bold">
+                                    {{-- <td>Total {{ $conta }}</td> --}}
+                                    <td></td>
+                                    <td></td>
+                                    <td class="text-right">{{ number_format($totalEntradas, 2, ',', '.') }}</td>
+                                    <td class="text-right">{{ number_format($totalSaidas, 2, ',', '.') }}</td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <button class="btn btn-success btn-rounded" onclick="exportReportToExcel();"><i class="fa fa-file-excel" aria-hidden="true"></i> Exportar</button>
+                    </div>
+                </div>
+            </div>
+            @endif
 
     @section('extras-scripts')
-    <script>
-        $(document).ready(function() {
-            $('#btn_relatorio').on('click', function() {
-                var dt_inicial = $('#dt_inicial').val();
-                var dt_final = $('#dt_final').val();
-                var caixa_id = $('#caixa_id').val();
+        <script>
+            $(document).ready(function() {
+                $('#btn_relatorio').on('click', function() {
+                    var dt_inicial = $('#dt_inicial').val();
+                    var dt_final = $('#dt_final').val();
+                    var caixa_id = $('#caixa_id').val();
 
-                if (!dt_inicial || !dt_final) {
-                    alert('Por favor, preencha todos os campos obrigatórios.');
-                    return;
-                }
+                    if (!dt_inicial || !dt_final) {
+                        alert('Por favor, preencha todos os campos obrigatórios.');
+                        return;
+                    }
 
-                var url = '{{ url("financeiro/relatorio/livrorazao/pdf") }}' + '?dt_inicial=' + dt_inicial + '&dt_final=' + dt_final + '&caixa_id=' + caixa_id;
-                window.open(url, '_blank');
+                    var url = '{{ url("financeiro/relatorio/livrorazao/pdf") }}' + '?dt_inicial=' + dt_inicial + '&dt_final=' + dt_final + '&caixa_id=' + caixa_id;
+                    window.open(url, '_blank');
+                });
+
+                $('#filter_form').submit(function(event) {
+                    var dt_inicial = $('#dt_inicial').val();
+                    var dt_final = $('#dt_final').val();
+
+                    if (!dt_inicial || !dt_final) {
+                        event.preventDefault();
+                        alert('Por favor, preencha todos os campos obrigatórios.');
+                    }
+                });
             });
-
-            $('#filter_form').submit(function(event) {
-                var dt_inicial = $('#dt_inicial').val();
-                var dt_final = $('#dt_final').val();
-
-                if (!dt_inicial || !dt_final) {
-                    event.preventDefault();
-                    alert('Por favor, preencha todos os campos obrigatórios.');
-                }
-            });
-        });
-    </script>
-    <script src="{{ asset('theme/assets/js/planilha/papaparse.min.js') }}"></script>
-    <script src="{{ asset('theme/assets/js/planilha/FileSaver.min.js') }}"></script>
-    <script src="{{ asset('theme/assets/js/planilha/xlsx.full.min.js') }}"></script>
-    <script src="{{ asset('theme/assets/js/planilha/planilha.js') }}"></script>
-    <script src="{{ asset('theme/assets/js/pages/movimentocaixa.js') }}"></script>
+        </script>
+        <script src="{{ asset('theme/assets/js/planilha/papaparse.min.js') }}"></script>
+        <script src="{{ asset('theme/assets/js/planilha/FileSaver.min.js') }}"></script>
+        <script src="{{ asset('theme/assets/js/planilha/xlsx.full.min.js') }}"></script>
+        <script src="{{ asset('theme/assets/js/planilha/planilha.js') }}"></script>
+        <script src="{{ asset('theme/assets/js/pages/movimentocaixa.js') }}"></script>
     @endsection
-    @endsection
+@endsection
