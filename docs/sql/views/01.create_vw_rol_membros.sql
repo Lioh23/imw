44751739563738ee078afc48568bcac8e5d2d703
Aurélio Silva de Jesus <aurelin95@gmail.com>
2024-08-03 10:24:25 -0300

    fix: vw_rol_membros

--- docs/sql/views/01.create_vw_rol_membros.sql
+++ docs/sql/views/01.create_vw_rol_membros.sql
@@ -7,6 +7,7 @@ CREATE OR REPLACE VIEW vw_rol_membros AS
 		,mr.distrito_id
 		,mr.igreja_id
 		,mm.congregacao_id
+		,cc.nome congregacao
 		,mr.status
 		,mm.data_nascimento
 		,mr.dt_recepcao 
@@ -14,12 +15,13 @@ CREATE OR REPLACE VIEW vw_rol_membros AS
 		,nt.id notificacao_transferencia_id
 		,mm.has_errors
 	
-  	  FROM  membresia_rolpermanente mr
-		
+ FROM (	SELECT *, ROW_NUMBER() OVER (PARTITION BY membro_id, igreja_id ORDER BY dt_recepcao DESC) as rn
+				FROM membresia_rolpermanente
+			) mr
+
 INNER JOIN membresia_membros mm
 	    ON mm.id = mr.membro_id
 	   AND mm.vinculo = 'M'
-	   AND mr.lastrec = 1
 	   
  LEFT JOIN congregacoes_congregacoes cc 
  		ON cc.id = mm.congregacao_id 
@@ -31,4 +33,6 @@ INNER JOIN membresia_membros mm
  	   AND nt.deleted_at IS NULL
  
 LEFT JOIN instituicoes_instituicoes nt_id
-	   ON nt_id.id = nt.igreja_destino_id;
+	   ON nt_id.id = nt.igreja_destino_id
+
+WHERE rn = 1;
