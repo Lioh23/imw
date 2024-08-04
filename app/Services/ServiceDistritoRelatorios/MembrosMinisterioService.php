2b621a259d208a16889d6ead0ece506a63990526
Prof Vinicius <gvweb.solucoes@gmail.com>
2024-08-03 23:23:39 -0300

    Membros

--- app/Services/ServiceDistritoRelatorios/MembrosMinisterioService.php
+++ app/Services/ServiceDistritoRelatorios/MembrosMinisterioService.php
@@ -44,76 +44,76 @@ class MembrosMinisterioService
             ->selectRaw("
                 COUNT(CASE 
                     WHEN TIMESTAMPDIFF(YEAR, mm.data_nascimento, CURDATE()) BETWEEN 0 AND 9 
-                        AND mr.dt_recepcao <= '{$dataInicial}' AND (mr.dt_exclusao IS NULL OR mr.dt_exclusao >= '{$dataInicial}') THEN mm.id
+                        AND mr.dt_recepcao <= '{$dataInicial}' AND mr.lastrec = 1 AND (mr.dt_exclusao IS NULL OR mr.dt_exclusao <= '{$dataInicial}') THEN mm.id
                     ELSE NULL
                 END) AS Kids_X,
                 COUNT(CASE 
                     WHEN TIMESTAMPDIFF(YEAR, mm.data_nascimento, CURDATE()) BETWEEN 0 AND 9 
-                        AND mr.dt_recepcao <= '{$dataFinal}' AND (mr.dt_exclusao IS NULL OR mr.dt_exclusao >= '{$dataFinal}') THEN mm.id
+                        AND mr.dt_recepcao <= '{$dataFinal}' AND mr.lastrec = 1 AND (mr.dt_exclusao IS NULL OR mr.dt_exclusao <= '{$dataFinal}') THEN mm.id
                     ELSE NULL
                 END) AS Kids_Y,
                 COUNT(CASE 
                     WHEN TIMESTAMPDIFF(YEAR, mm.data_nascimento, CURDATE()) BETWEEN 10 AND 13 
-                        AND mr.dt_recepcao <= '{$dataInicial}' AND (mr.dt_exclusao IS NULL OR mr.dt_exclusao >= '{$dataInicial}') THEN mm.id
+                        AND mr.dt_recepcao <= '{$dataInicial}' AND mr.lastrec = 1 AND (mr.dt_exclusao IS NULL OR mr.dt_exclusao <= '{$dataInicial}') THEN mm.id
                     ELSE NULL
                 END) AS Conexao_X,
                 COUNT(CASE 
                     WHEN TIMESTAMPDIFF(YEAR, mm.data_nascimento, CURDATE()) BETWEEN 10 AND 13 
-                        AND mr.dt_recepcao <= '{$dataFinal}' AND (mr.dt_exclusao IS NULL OR mr.dt_exclusao >= '{$dataFinal}') THEN mm.id
+                        AND mr.dt_recepcao <= '{$dataFinal}' AND mr.lastrec = 1 AND (mr.dt_exclusao IS NULL OR mr.dt_exclusao <= '{$dataFinal}') THEN mm.id
                     ELSE NULL
                 END) AS Conexao_Y,
                 COUNT(CASE 
                     WHEN TIMESTAMPDIFF(YEAR, mm.data_nascimento, CURDATE()) BETWEEN 14 AND 17 
-                        AND mr.dt_recepcao <= '{$dataInicial}' AND (mr.dt_exclusao IS NULL OR mr.dt_exclusao >= '{$dataInicial}') THEN mm.id
+                        AND mr.dt_recepcao <= '{$dataInicial}' AND mr.lastrec = 1 AND (mr.dt_exclusao IS NULL OR mr.dt_exclusao <= '{$dataInicial}') THEN mm.id
                     ELSE NULL
                 END) AS Fire_X,
                 COUNT(CASE 
                     WHEN TIMESTAMPDIFF(YEAR, mm.data_nascimento, CURDATE()) BETWEEN 14 AND 17 
-                        AND mr.dt_recepcao <= '{$dataFinal}' AND (mr.dt_exclusao IS NULL OR mr.dt_exclusao >= '{$dataFinal}') THEN mm.id
+                        AND mr.dt_recepcao <= '{$dataFinal}' AND mr.lastrec = 1 AND (mr.dt_exclusao IS NULL OR mr.dt_exclusao <= '{$dataFinal}') THEN mm.id
                     ELSE NULL
                 END) AS Fire_Y,
                 COUNT(CASE 
                     WHEN TIMESTAMPDIFF(YEAR, mm.data_nascimento, CURDATE()) BETWEEN 18 AND 29 
-                        AND mr.dt_recepcao <= '{$dataInicial}' AND (mr.dt_exclusao IS NULL OR mr.dt_exclusao >= '{$dataInicial}') THEN mm.id
+                        AND mr.dt_recepcao <= '{$dataInicial}' AND mr.lastrec = 1 AND (mr.dt_exclusao IS NULL OR mr.dt_exclusao <= '{$dataInicial}') THEN mm.id
                     ELSE NULL
                 END) AS Move_X,
                 COUNT(CASE 
                     WHEN TIMESTAMPDIFF(YEAR, mm.data_nascimento, CURDATE()) BETWEEN 18 AND 29 
-                        AND mr.dt_recepcao <= '{$dataFinal}' AND (mr.dt_exclusao IS NULL OR mr.dt_exclusao >= '{$dataFinal}') THEN mm.id
+                        AND mr.dt_recepcao <= '{$dataFinal}' AND mr.lastrec = 1 AND (mr.dt_exclusao IS NULL OR mr.dt_exclusao <= '{$dataFinal}') THEN mm.id
                     ELSE NULL
                 END) AS Move_Y,
                 COUNT(CASE 
                     WHEN TIMESTAMPDIFF(YEAR, mm.data_nascimento, CURDATE()) BETWEEN 30 AND 59 
                         AND mm.sexo = 'M' 
-                        AND mr.dt_recepcao <= '{$dataInicial}' AND (mr.dt_exclusao IS NULL OR mr.dt_exclusao >= '{$dataInicial}') THEN mm.id
+                        AND mr.dt_recepcao <= '{$dataInicial}' AND mr.lastrec = 1 AND (mr.dt_exclusao IS NULL OR mr.dt_exclusao <= '{$dataInicial}') THEN mm.id
                     ELSE NULL
                 END) AS Homens_X,
                 COUNT(CASE 
                     WHEN TIMESTAMPDIFF(YEAR, mm.data_nascimento, CURDATE()) BETWEEN 30 AND 59 
                         AND mm.sexo = 'M' 
-                        AND mr.dt_recepcao <= '{$dataFinal}' AND (mr.dt_exclusao IS NULL OR mr.dt_exclusao >= '{$dataFinal}') THEN mm.id
+                        AND mr.dt_recepcao <= '{$dataFinal}' AND mr.lastrec = 1 AND (mr.dt_exclusao IS NULL OR mr.dt_exclusao <= '{$dataFinal}') THEN mm.id
                     ELSE NULL
                 END) AS Homens_Y,
                 COUNT(CASE 
                     WHEN TIMESTAMPDIFF(YEAR, mm.data_nascimento, CURDATE()) BETWEEN 30 AND 59 
                         AND mm.sexo = 'F' 
-                        AND mr.dt_recepcao <= '{$dataInicial}' AND (mr.dt_exclusao IS NULL OR mr.dt_exclusao >= '{$dataInicial}') THEN mm.id
+                        AND mr.dt_recepcao <= '{$dataInicial}' AND mr.lastrec = 1 AND (mr.dt_exclusao IS NULL OR mr.dt_exclusao <= '{$dataInicial}') THEN mm.id
                     ELSE NULL
                 END) AS Mulheres_X,
                 COUNT(CASE 
                     WHEN TIMESTAMPDIFF(YEAR, mm.data_nascimento, CURDATE()) BETWEEN 30 AND 59 
                         AND mm.sexo = 'F' 
-                        AND mr.dt_recepcao <= '{$dataFinal}' AND (mr.dt_exclusao IS NULL OR mr.dt_exclusao >= '{$dataFinal}') THEN mm.id
+                        AND mr.dt_recepcao <= '{$dataFinal}' AND mr.lastrec = 1 AND (mr.dt_exclusao IS NULL OR mr.dt_exclusao <= '{$dataFinal}') THEN mm.id
                     ELSE NULL
                 END) AS Mulheres_Y,
                 COUNT(CASE 
                     WHEN TIMESTAMPDIFF(YEAR, mm.data_nascimento, CURDATE()) >= 60 
-                        AND mr.dt_recepcao <= '{$dataInicial}' AND (mr.dt_exclusao IS NULL OR mr.dt_exclusao >= '{$dataInicial}') THEN mm.id
+                        AND mr.dt_recepcao <= '{$dataInicial}' AND mr.lastrec = 1 AND (mr.dt_exclusao IS NULL OR mr.dt_exclusao <= '{$dataInicial}') THEN mm.id
                     ELSE NULL
                 END) AS Sessenta_X,
                 COUNT(CASE 
                     WHEN TIMESTAMPDIFF(YEAR, mm.data_nascimento, CURDATE()) >= 60 
-                        AND mr.dt_recepcao <= '{$dataFinal}' AND (mr.dt_exclusao IS NULL OR mr.dt_exclusao >= '{$dataFinal}') THEN mm.id
+                        AND mr.dt_recepcao <= '{$dataFinal}' AND mr.lastrec = 1 AND (mr.dt_exclusao IS NULL OR mr.dt_exclusao <= '{$dataFinal}') THEN mm.id
                     ELSE NULL
                 END) AS Sessenta_Y
             ")
