6730c5e1699b492ff7f7cda61bae54859446f94e
Prof Vinicius <gvweb.solucoes@gmail.com>
2024-08-07 00:19:02 -0300

    SALDO IGREHAS

--- app/Services/ServiceDistritoRelatorios/LivroRazaoGeralService.php
+++ app/Services/ServiceDistritoRelatorios/LivroRazaoGeralService.php
@@ -60,7 +60,7 @@ class LivroRazaoGeralService
                 total_entradas > 0 OR total_saidas > 0 OR total > 0
             ORDER BY 
                 pc.numeracao ASC,
-                ii.data_movimentacao ASC
+                fl.data_movimento ASC
         ",
         ['dataInicial' => $dataInicial, 'dataFinal' => $dataFinal, 'instituicaoPaiId' => $instituicaoPaiId]);
     
