49bb84c0cf41c410cfd99bcbfa7d32c81985f774
Prof Vinicius <gvweb.solucoes@gmail.com>
2024-08-06 23:18:15 -0300

    SALDO IGREHAS

--- resources/views/distrito/relatorios/saldodasigrejas_pdf.blade.php
+++ resources/views/distrito/relatorios/saldodasigrejas_pdf.blade.php
@@ -99,9 +99,11 @@
     <table>
         <thead>
             <tr>
-                <th width="200" style="text-align: left">IGREJA</th>
+                <th width="150" style="text-align: left">IGREJA</th>
                 <th width="150" style="text-align: right">SALDO CAIXA PRINCIPAL</th>
-                <th width="150" style="text-align: right">SALDO CAIXA CONGREGAÇÕES</th>
+                <th width="180" style="text-align: right">SALDO CAIXA CONGREGAÇÕES</th>
+                <th width="150" style="text-align: right">SALDO CAIXA SECUNDÁRIO</th>
+                <th width="150" style="text-align: right">SALDO CAIXA BANCOS</th>
                 <th width="150" style="text-align: right">SALDO OUTROS CAIXAS</th>
                 <th width="70" style="text-align: right">TOTAL</th>
             </tr>
@@ -113,6 +115,8 @@
                 <td style="text-align: right">{{ number_format($lancamento->saldocxprincipal, 2, ',', '.') }}</td>
                 <td style="text-align: right">{{ number_format($lancamento->saldocxcongregacoes, 2, ',', '.') }}</td>
                 <td style="text-align: right">{{ number_format($lancamento->saldocxsecundado, 2, ',', '.') }}</td>
+                <td style="text-align: right">{{ number_format($lancamento->saldocxbancos, 2, ',', '.') }}</td>
+                <td style="text-align: right">{{ number_format($lancamento->saldocxoutros, 2, ',', '.') }}</td>
                 <td style="text-align: right">{{ number_format($lancamento->total, 2, ',', '.') }}</td>
             </tr>
             @endforeach
