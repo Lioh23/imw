0ca26f11c0a9799d74e8070a57eb54e13de5f2a7
Prof Vinicius <gvweb.solucoes@gmail.com>
2024-09-28 13:51:27 -0300

    Igrejas

--- app/Http/Controllers/InstituicaoRegiaoIgrejasController.php
+++ app/Http/Controllers/InstituicaoRegiaoIgrejasController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Services\ServiceInstituicaoIgrejas\ListarIgrejasDistritosServices;
 use Illuminate\Http\Request;
 
 class InstituicaoRegiaoIgrejasController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $searchTerm = $request->input('search');
+        $igrejas = app(ListarIgrejasDistritosServices::class)->execute($searchTerm);
+        return view('instituicoes.igrejas.index', compact('igrejas'));
+    }
 }
