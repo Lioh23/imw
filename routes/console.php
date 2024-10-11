82b5a47b16dbe41dc49976653ba0b8c1836894a5
Jose Miguel <migueljcbrbm@gmail.com>
2024-10-11 16:38:10 -0300

    Igrejas Regional

--- routes/console.php
+++ routes/console.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\InstituicoesInstituicao;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('identificar:regiao', function ( ) {
+    dd(InstituicoesInstituicao::all()->take(10));
+});
