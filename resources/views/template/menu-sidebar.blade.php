92af46246272b4da20e7f8f3a40be016be1345ee
Jose Miguel <migueljcbrbm@gmail.com>
2024-10-23 09:21:47 -0300

    Ajustando rota

--- resources/views/template/menu-sidebar.blade.php
+++ resources/views/template/menu-sidebar.blade.php
@@ -143,7 +143,7 @@
 
              @if (auth()->check() && auth()->user()->hasPerfilRegra('menu-instituicoes'))
                  <li class="menu">
-                     <a href="{{ route('instituicoes.index') }}"class="dropdown-toggle">
+                     <a href="{{ route('instituicoes-regiao.index') }}"class="dropdown-toggle">
                          <div class="">
                              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                  viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
