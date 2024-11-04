b6b4656f48cceed116cfc9bd41a20c6c912e5c41
Jose Miguel <migueljcbrbm@gmail.com>
2024-11-04 10:03:20 -0300

    Resolvendo problema do Search em clerigos 2

--- app/Services/ServiceClerigosRegiao/ListaClerigosService.php
+++ app/Services/ServiceClerigosRegiao/ListaClerigosService.php
@@ -12,7 +12,6 @@ class ListaClerigosService
         
         $regiao_id = (int) session()->get('session_perfil')->instituicao_id;
         $searchTerm = is_array($parameters) && isset($parameters['searchTerm']) ? $parameters['searchTerm'] : null;
-        dd($parameters);
         $clerigos = PessoasPessoa::query()->withTrashed()->where('regiao_id', $regiao_id)
             ->when($searchTerm, function ($query, $searchTerm) {
                 $query->where('nome', 'like', "%{$searchTerm}%");
