8d6b519907de4db8b9aebc7261335a8710b21a20
Aurélio Silva de Jesus <aurelin95@gmail.com>
2024-11-18 18:46:32 -0300

    feat: classname

--- app/Models/PessoaFuncaoministerial.php
+++ app/Models/PessoaFuncaoministerial.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class PessoaFuncaoministeral extends Model
+class PessoaFuncaoministerial extends Model
 {
     use HasFactory;
 
