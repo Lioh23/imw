15076aa235b61910e306983c4f9d136f66a0ef06
Aurélio Silva de Jesus <aurelin95@gmail.com>
2024-12-17 21:34:23 -0300

    wip: prebendas

---
+++ database/migrations/2024_12_16_221310_create_prebendas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prebendas', function (Blueprint $table) {
+            $table->id();
+            $table->integer('ano');
+            $table->decimal('valor', 12, 2);
+            $table->boolean('ativo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prebendas');
+    }
+};
