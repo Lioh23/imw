5aeaabe78d28e2a5e046414dbde9f224a3c14c11
Jose Miguel <migueljcbrbm@gmail.com>
2025-01-12 11:43:00 -0300

    fix:Tirando obrigatoriaridade dos campos CTPS e data_CPTS em clerigos

--- app/Http/Requests/StoreReceberNovoClerigoRequest.php
+++ app/Http/Requests/StoreReceberNovoClerigoRequest.php
@@ -83,8 +83,6 @@ class StoreReceberNovoClerigoRequest extends FormRequest
             'sexo.required' => 'O campo sexo é obrigatório.',
             'escolaridade.required' => 'O campo escolaridade é obrigatório.',
             'telefone_preferencial.required' => 'O campo telefone preferencial é obrigatório.',
-            'ctps.required' => 'O campo CTPS é obrigatório.',
-            'ctps_emissao.required' => 'O campo data de emissão da CTPS é obrigatório.',
             'titulo_eleitor.required' => 'O campo título de eleitor é obrigatório.',
             'titulo_eleitor_secao.required' => 'O campo seção do título de eleitor é obrigatório.',
             'titulo_eleitor_zona.required' => 'O campo zona do título de eleitor é obrigatório.',
