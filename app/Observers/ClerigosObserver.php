3f041a9ca3020610cdc235008ae64851aac70f63
Jose Miguel <migueljcbrbm@gmail.com>
2025-01-29 15:28:12 -0300

    feat: Observer para tirar caracteres especiais para create e update de clerigos

---
+++ app/Observers/ClerigosObserver.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\PessoasPessoa;
+
+class ClerigosObserver
+{
+    public function creating(PessoasPessoa $pessoasPessoas)
+    {
+
+
+        if ($pessoasPessoas->cep) {
+            $pessoasPessoas->cep = preg_replace('/[^\d]/', '', $pessoasPessoas->cep);
+        }
+
+        if ($pessoasPessoas->cpf) {
+            $pessoasPessoas->cpf = preg_replace('/[^\d]/', '', $pessoasPessoas->cpf);
+        }
+
+        if ($pessoasPessoas->telefone_preferencial) {
+            $pessoasPessoas->telefone_preferencial = preg_replace('/[^\d]/', '', $pessoasPessoas->telefone_preferencial);
+        }
+
+        if ($pessoasPessoas->celular) {
+            $pessoasPessoas->celular = preg_replace('/[^\d]/', '', $pessoasPessoas->celular);
+        }
+    }
+
+    public function updating(PessoasPessoa $pessoasPessoas)
+    {
+
+        if ($pessoasPessoas->cep) {
+            $pessoasPessoas->cep = preg_replace('/[^\d]/', '', $pessoasPessoas->cep);
+        }
+
+        if ($pessoasPessoas->cpf) {
+            $pessoasPessoas->cpf = preg_replace('/[^\d]/', '', $pessoasPessoas->cpf);
+        }
+
+        if ($pessoasPessoas->telefone_preferencial) {
+            $pessoasPessoas->telefone_preferencial = preg_replace('/[^\d]/', '', $pessoasPessoas->telefone_preferencial);
+        }
+
+        if ($pessoasPessoas->celular) {
+            $pessoasPessoas->celular = preg_replace('/[^\d]/', '', $pessoasPessoas->celular);
+        }
+    }
+}
