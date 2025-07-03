bdcd37b21b8260a05c55f62b21c35e497e91e0cd
Davi Bernardo <dnssites@gmail.com>
2025-07-02 21:54:38 -0300

    Finalizando o relatório de clerigos aniversariantes

--- docs/sql/regra-relatorios-instituicoes-igrejas-aniversariantes30-06-2026.sql
+++ docs/sql/regra-relatorios-instituicoes-igrejas-aniversariantes30-06-2026.sql
@@ -1,5 +1,5 @@
-INSERT INTO `regras` (`id`, `nome`, `created_at`, `updated_at`, `deleted_at`) VALUES (130, 'menu-relatorios-instituicoes-igrejas', '2025-06-30 18:29:04', '2025-06-30 18:29:04', NULL);
-INSERT INTO `regras` (`id`, `nome`, `created_at`, `updated_at`, `deleted_at`) VALUES(131, 'relatorio-clerigos-aniversariantes', '2025-06-30 18:29:04', '2025-06-29 30:29:04', NULL);
+INSERT INTO `regras` (`id`, `nome`, `created_at`, `updated_at`, `deleted_at`) VALUES(NULL, 'relatorio-clerigos-aniversariantes', '2025-06-30 18:29:04', '2025-06-29 30:29:04', NULL);
+INSERT INTO `regras` (`id`, `nome`, `created_at`, `updated_at`, `deleted_at`) VALUES (NULL, 'menu-relatorios-instituicoes-igrejas', '2025-06-30 18:29:04', '2025-06-30 18:29:04', NULL);
 
 INSERT INTO `perfil_regra` (`id`, `perfil_id`, `regra_id`, `created_at`, `updated_at`) VALUES
 (NULL, 3, 131, '2025-06-30 18:35:52', '2025-06-30 18:35:52'),
