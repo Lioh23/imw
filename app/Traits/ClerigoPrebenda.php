de408ec7d9dec4126631cf940a518a96319b7898
Davi Bernardo <dnssites@gmail.com>
2025-07-28 20:45:41 -0300

    Melhorias no relatório clerigo status

--- app/Traits/ClerigoPrebenda.php
+++ app/Traits/ClerigoPrebenda.php
@@ -136,20 +136,20 @@ trait ClerigoPrebenda
                 DB::raw("CASE WHEN telefone_preferencial IS NOT NULL AND telefone_preferencial <> '' THEN telefone_preferencial
                               WHEN telefone_alternativo IS NOT NULL AND telefone_alternativo <> '' THEN telefone_alternativo
                               ELSE '' END contato"),
-                DB::raw("(SELECT CONCAT(iii.nome) FROM instituicoes_instituicoes iii WHERE iii.id = pp.igreja_id AND iii.ativo = 1) igreja")
+                DB::raw("(SELECT CONCAT(iii.nome) FROM instituicoes_instituicoes iii WHERE iii.id = pp.igreja_id) igreja")
             )
             ->leftJoin('pessoas_status as ps', function ($join) {
                 $join->on('ps.id', '=', 'pp.status_id');
             })
-            ->join('pessoas_nomeacoes as pn', function ($join) {
+            ->leftJoin('pessoas_nomeacoes as pn', function ($join) {
                 $join->on('pp.id', '=', 'pn.pessoa_id')
                     ->whereNull('pn.data_termino');
             })
-            ->join('instituicoes_instituicoes as ii', function ($join) {
-                $join->on('pn.instituicao_id', '=', 'ii.id')->where('ii.ativo', '=', 1);
+            ->leftJoin('instituicoes_instituicoes as ii', function ($join) {
+                $join->on('pn.instituicao_id', '=', 'ii.id');
             })
             ->when($params['status'], fn ($query) => $query->where('pp.status_id', $params['status']))
-            ->where([/*'pp.status_id' => 1,*/ 'ii.ativo' => 1, 'pp.regiao_id' => $regiao, 'pn.data_termino' => null])
+            ->where([/*'pp.status_id' => 1,*/ 'pp.regiao_id' => $regiao, 'pn.data_termino' => null])
             ->orderBy('pp.nome')
             ->groupBy('id', 'pp.nome', 'pp.telefone_preferencial', 'pp.telefone_alternativo', 'pp.igreja_id', 'ps.descricao')
             ->get();        
