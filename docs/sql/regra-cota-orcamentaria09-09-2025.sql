626393709145635c1e51dc1fd411238e54f5f56c
Davi Bernardo <dnssites@gmail.com>
2025-10-03 20:51:51 -0300

    Melhorias no sql do relatório cota orçamentária

--- docs/sql/regra-cota-orcamentaria09-09-2025.sql
+++ docs/sql/regra-cota-orcamentaria09-09-2025.sql
@@ -3,3 +3,7 @@ INSERT INTO `regras` (`id`, `nome`, `created_at`, `updated_at`, `deleted_at`) VA
 INSERT INTO `perfil_regra` (`id`, `perfil_id`, `regra_id`, `created_at`, `updated_at`) VALUES (NULL, '3', '148', '2025-09-09 19:11:34', '2025-09-09 19:11:34');
 
 INSERT INTO `perfil_regra` (`id`, `perfil_id`, `regra_id`, `created_at`, `updated_at`) VALUES (NULL, '1', '148', '2025-09-09 19:11:34', '2025-09-09 19:11:34');
+
+UPDATE `perfil_regra` SET `perfil_id` = '7' WHERE `perfil_regra`.`id` = 1141;
+
+INSERT INTO `perfil_regra` (`id`, `perfil_id`, `regra_id`, `created_at`, `updated_at`) VALUES (NULL, '5', '148', '2025-10-03 20:44:34', '2025-10-03 20:44:34');
--- docs/sql/regra-distrito-cota-orcamentaria03-10-2025.sql
+++ docs/sql/regra-distrito-cota-orcamentaria03-10-2025.sql
@@ -3,3 +3,5 @@ INSERT INTO `regras` (`id`, `nome`, `created_at`, `updated_at`, `deleted_at`) VA
 INSERT INTO `perfil_regra` (`id`, `perfil_id`, `regra_id`, `created_at`, `updated_at`) VALUES (NULL, '1', '149', '2025-10-03 17:15:34', '2025-10-03 17:15:34');
 INSERT INTO `perfil_regra` (`id`, `perfil_id`, `regra_id`, `created_at`, `updated_at`) VALUES (NULL, '2', '149', '2025-10-03 17:15:34', '2025-10-03 17:15:34');
 INSERT INTO `perfil_regra` (`id`, `perfil_id`, `regra_id`, `created_at`, `updated_at`) VALUES (NULL, '9', '149', '2025-10-03 17:15:34', '2025-10-03 17:15:34');
+
+UPDATE `perfil_regra` SET `perfil_id` = '3' WHERE `perfil_regra`.`id` = 1143;
