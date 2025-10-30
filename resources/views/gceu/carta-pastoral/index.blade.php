fcdd16189e8eb02c1c7207196216389c6287c51c
Davi Bernardo <dnssites@gmail.com>
2025-10-29 21:47:53 -0300

    Finalizando o cadastro e relatóriodas cartas pastorais

--- resources/views/gceu/carta-pastoral/index.blade.php
+++ resources/views/gceu/carta-pastoral/index.blade.php
@@ -29,7 +29,7 @@
     <div class="container-fluid d-flex justify-content-between">
         {{-- esquerda --}}
         <div>
-            <a href="{{ route('gceu.carta-pastoral.novo', $instituicao_id) }}" class="btn btn-primary position-relative mt-3 mb-3 ml-2">
+            <a href="{{ route('gceu.carta-pastoral.novo') }}" class="btn btn-primary position-relative mt-3 mb-3 ml-2">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                     stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                     class="feather feather-plus-circle">
--- resources/views/gceu/carta-pastoral/slice-actions.blade.php
+++ resources/views/gceu/carta-pastoral/slice-actions.blade.php
@@ -6,7 +6,7 @@
         <i class="fa fa-file-pdf" style="font-size: 14px;"></i>
     </a>
 @if (!$cartaPastoral->deleted_at)
-    <a  href="{{ route('gceu.carta-pastoral.pdf', $cartaPastoral->id) }}"
+    <a  href="{{ route('gceu.carta-pastoral.editar', $cartaPastoral->id) }}"
         title="Editar"
         class="btn btn-sm btn-dark mr-2 btn-rounded bs-tooltip">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-2">
