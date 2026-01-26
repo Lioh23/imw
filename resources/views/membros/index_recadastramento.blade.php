eb5c0ab5e126c1d70df88342136b056ff7c7e509
Davi Bernardo <dnssites@gmail.com>
2026-01-26 18:41:58 -0300

    Ocultando o menu Membresia Validação

--- resources/views/membros/index_recadastramento.blade.php
+++ resources/views/membros/index_recadastramento.blade.php
@@ -43,7 +43,7 @@
             <div class="widget-header">
                 <div class="row">
                     <div class="col-xl-12 col-md-12 col-sm-12 col-12">
-                        <h4>Lista de Membros Recadastramento</h4>
+                        <h4>Lista de Membresia Validação</h4>
                     </div>
                 </div>
             </div>
